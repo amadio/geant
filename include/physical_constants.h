fa65ebc7aa801d59390bb6aad1de4b9571803122
John Apostolakis <John.Apostolakis@cern.ch>
2012-04-02 16:02:18 +0000

    Some necessary constants, units.
    git-svn-id: https://root.cern.ch/svn/geant/trunk@26 086b3c04-5c6c-469f-ab82-383f8242ccf9

---
+++ include/physical_constants.h
@@ -0,0 +1,9 @@
+
+static const  Double_t  PI  = 3.14159265358979323846;
+
+// Units
+static const  Double_t  MeV  = 1.0; 
+static const  Double_t  keV = 0.001 * MeV; 
+
+// Physical constants 
+static const  Double_t  electron_mass_c2 = 0.510998910 * MeV;
