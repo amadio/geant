e12e373e7aee50c62b30402e917acb502e569076
Federico Carminati <federico.carminati@cern.ch>
2013-06-05 11:58:33 +0200

    Now generating the dictionary

---
+++ x-secs/include/TXsecLinkDef.h
@@ -0,0 +1,23 @@
+/* @(#)root/base:$Id: LinkDef2.h 42250 2011-11-25 17:30:31Z pcanal $ */
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CINT__
+
+#ifdef __CLING__
+#include <string>
+#else
+#include "dll_stl/str.h" 	 
+#endif
+
+
+#pragma link C++ class TXMsec+;
+#pragma link C++ class TXPsec+;
+
+#endif
