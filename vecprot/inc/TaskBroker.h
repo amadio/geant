d7ad273f781a127b4f0ffa85ea12a27c96c81025
Philippe Canal <pcanal@fnal.gov>
2013-06-20 15:36:02 -0500

    Mark all member function as abstract

--- vecprot/inc/TaskBroker.h
+++ vecprot/inc/TaskBroker.h
@@ -13,10 +13,10 @@ public:
 
    virtual void runTask(int threadid, int nTracks, int volumeIndex, GeantTrack **tracks, int *trackin) = 0;
 
-   virtual Stream launchTask(bool wait = false);
-   virtual void waitForTasks();
+   virtual Stream launchTask(bool wait = false) = 0;
+   virtual void waitForTasks() = 0;
    
-   virtual long GetTotalWork();
+   virtual long GetTotalWork() = 0;
 };
 
 
