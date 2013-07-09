dba2ab002bbdcf74315ca49597818d1f79165d90
Sandro Wenzel <sandro.wenzel@cern.ch>
2013-07-09 14:21:00 +0200

    renamed and fixed test for polycone

---
+++ geom_vec_tests/src/BenchmarkFramework/BenchTGeoPcon_v.cxx
@@ -0,0 +1,24 @@
+#include "TGeoPcon_v.h"
+#include "VecPerformanceTesterT.h" // for the vectorized interface
+#include "TGeoManager.h"
+#include "TObjArray.h"
+#include "TGeoPcon.h"
+#include <iostream>
+
+//  this takes a couple of shapes from the CMS detector description 
+//  and performs the standard benchmark
+//  we know here ( study of volume list ), that
+//  volume 2 = TGeoPcone
+
+
+int main(int argc, char *argv[])
+{
+  TGeoManager::Import("cms.root");
+  TObjArray *vlist= gGeoManager->GetListOfVolumes();
+  TGeoPcon_v *apcon = (TGeoPcon_v *)( ((TGeoVolume*) vlist->UncheckedAt(2))->GetShape());
+
+  // test vector performance for Pcon
+  apcon->InspectShape();
+  ShapeBenchmarker_v<TGeoPcon_v> testpcon( apcon );
+  testpcon.timeIt();
+}
