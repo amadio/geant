59985427ec867c3735ee8d3a0a8c6e10a35b4942
Sandro Wenzel <sandro.wenzel@cern.ch>
2013-07-10 11:46:59 +0200

    added features to RDTSC timer

--- geom_vec_tests/inc/RDTSCStopWatch.h
+++ geom_vec_tests/inc/RDTSCStopWatch.h
@@ -1,4 +1,4 @@
-#ifndef RDTSCTOPWATCH_H
+#ifndef RDTSCSTOPWATCH_H
 #define RDTSCSTOPWATCH_H
 
 // A "timer" based on CPU cycles which can be read out with the rdtsc instruction
@@ -14,6 +14,20 @@ static inline unsigned long long rdtsc()
   __asm__ __volatile__ (
       "xorl %%eax, %%eax\n"
       "cpuid\n"
+      "xorl %%eax, %%eax\n"
+      "cpuid\n"
+      "rdtsc\n"
+      : "=a" (lo), "=d" (hi)
+      :
+      : "%ebx", "%ecx" );
+  return (unsigned long long)hi << 32 | lo;
+}
+
+// does not forbid out-of-order execution -- probably ok after a function call return?
+static inline unsigned long long rdtsc_nocpuid()
+{
+  unsigned int lo, hi;
+  __asm__ __volatile__ (
       "rdtsc\n"
       : "=a" (lo), "=d" (hi)
       :
@@ -22,13 +36,15 @@ static inline unsigned long long rdtsc()
 }
 
 
+
 struct RDTSCStopWatch 
 {
-  static const double inversefreq=1./(3.40017e9); // ( for my 3.4 GHz machine )
+  static const double inversefreq=1./(3.40017e9); // ( for my 3.4 GHz machine, taken from /proc/cpuinfo )
   unsigned long long t1;
   unsigned long long t2;
   void Start(){ t1= rdtsc(); }
   void Stop(){  t2= rdtsc(); }
+  void Stop_nocpuid(){  t2= rdtsc_nocpuid(); }
 
   void HeatUp(){ for(unsigned int i=0;i<5;++i){ Start();Stop(); } }
 
