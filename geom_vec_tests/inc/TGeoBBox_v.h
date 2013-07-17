8b6a9b97b84ba5575d1f5c00804052c0d008d246
Sandro Wenzel <sandro.wenzel@cern.ch>
2013-07-17 16:53:09 +0200

    modification to CMakeFiles (adding dependencies for more stable building with make -jN); added static library build for libVecGeom

--- geom_vec_tests/inc/TGeoBBox_v.h
+++ geom_vec_tests/inc/TGeoBBox_v.h
@@ -104,11 +104,12 @@ public:
      TGeoBBox_v::DistFromOutsideS_v(point, dir, this->TGeoBBox::GetDX(), this->TGeoBBox::GetDY(), this->TGeoBBox::GetDZ(), this->TGeoBBox::GetOrigin(),  step, distance, np );
    }
 
+#ifdef VEC_EXTENSIONS
    virtual void    DistFromOutside_Masked_v(double const *m, StructOfCoord const & point, StructOfCoord const & dir, Int_t iact, 
 				     Double_t const * step, Double_t *safe, Double_t * distance , Int_t np) const {
      TGeoBBox_v::DistFromOutsideS_Masked_v(m, point, dir, this->TGeoBBox::GetDX(), this->TGeoBBox::GetDY(), this->TGeoBBox::GetDZ(), this->TGeoBBox::GetOrigin(),  step, distance, np );
    }
-
+#endif
 
    // static version ( added "S" to it to make it consistent with other TGeo classes )
    static  Double_t  DistFromOutsideS(const Double_t *point,const Double_t *dir,
@@ -120,9 +121,11 @@ public:
    static  void      DistFromOutsideS_v(const StructOfCoord & point,const StructOfCoord & dir,
 					   Double_t dx, Double_t dy, Double_t dz, const Double_t *origin, const Double_t * stepmax, Double_t * distance, Int_t np);
   // SOA version for static method
+#ifdef VEC_EXTENSIONS
    static  void      DistFromOutsideS_Masked_v(double const*, const StructOfCoord & point,const StructOfCoord & dir,
 					   Double_t dx, Double_t dy, Double_t dz, const Double_t *origin, const Double_t * stepmax, Double_t * distance, Int_t np);
-   
+#endif   
+
    // SOA trivial loop version for static method
    static  void      DistFromOutsideS_l(const StructOfCoord & point,const StructOfCoord & dir,
 					   Double_t dx, Double_t dy, Double_t dz, const Double_t *origin, const Double_t * stepmax, Double_t * distance, Int_t np);
