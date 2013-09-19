21912ed2850c3c663a02566dcab4b954de56f798
mariba <mariba81@cerrigone.oact.inaf.it>
2013-09-19 19:15:45 +0200

    Geant-V TGeoCone_v commit

--- geom_vec_tests/inc/TGeoCone_v.h
+++ geom_vec_tests/inc/TGeoCone_v.h
@@ -1,33 +1,49 @@
 #ifndef ROOT_TGeoPcon_v
-#define ROOT_TGeoPcon_v 
+#define ROOT_TGeoPcon_v
 
 #ifndef ROOT_TGeoShape
 #include "TGeoShape.h"
 #endif
 #include "TGeoCone.h"
 
+#ifdef VEC_EXTENSIONS
+#include "Vc/vector.h"
+#endif
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "PointStruct.h" // for SOA data
 
-class TGeoCone_v: public TGeoCone
+class TGeoCone_v: public TGeoCone /*public TGeoConeSeg*/
 {
 public:
-  virtual void Contains_v(const StructOfCoord & pointi, Bool_t * isin, Int_t np) const; 
-  virtual void Safety_v(const StructOfCoord &pointi, Bool_t in, Double_t *safety, Int_t np ) const;         
-  virtual void DistFromInside_v(StructOfCoord const  & pointi, const StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const; 
-  virtual void DistFromOutside_v(const StructOfCoord & pointi, const  StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const; 
-
+    virtual void Contains_v(const StructOfCoord & pointi, Bool_t * isin, Int_t np) const;
+    
+    virtual void Safety_v(const StructOfCoord &pointi, Bool_t in, Double_t *safety, Int_t np ) const;
+    virtual void DistFromInside_v(StructOfCoord const  & pointi, const StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const;
+    virtual void DistFromOutside_v(const StructOfCoord & pointi, const  StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const;
+    //virtual void SafetySeg_v(Vc::double_v r, Vc::double_v z, Double_t r1,Double_t z1, Double_t r2, Double_t z2, Bool_t outer, Double_t * safetySeg, Int_t np ) const;
+    
+    
 };
 
-class TGeoConeSeg_v: public TGeoConeSeg
+class TGeoConeSeg_v: public TGeoConeSeg  /*public TGeoCone_v*/
 {
 public:
-  virtual void Contains_v(const StructOfCoord & pointi, Bool_t * isin, Int_t np) const; 
-  virtual void Safety_v(const StructOfCoord &pointi, Bool_t in, Double_t *safety, Int_t np ) const;         
-  virtual void DistFromInside_v(StructOfCoord const  & pointi, const StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const; 
-  virtual void DistFromOutside_v(const StructOfCoord & pointi, const  StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const; 
+    virtual void Contains_v(const StructOfCoord & pointi, Bool_t * isin, Int_t np) const;
+#ifdef VEC_EXTENSIONS
+private:
+    virtual void Contains_v4( Vc::double_v const & x , Vc::double_v const & y, Vc::double_v const & z,  Vc::double_m &c1);
+public:
+#endif
+    virtual void Safety_v(const StructOfCoord &pointi, Bool_t in, Double_t *safety, Int_t np ) const;
+    virtual void DistFromInside_v(StructOfCoord const  & pointi, const StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const;
+    virtual void DistFromOutside_v(const StructOfCoord & pointi, const  StructOfCoord & diri, Int_t iact, const Double_t * step, Double_t *safe, Double_t * distance , Int_t np) const;
+    
+    
 };
 
 #endif
+
--- geom_vec_tests/src/TGeoCone_v.cxx
+++ geom_vec_tests/src/TGeoCone_v.cxx
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "TGeoCone_v.h"
+#include "TMath.h"
+#ifdef VEC_EXTENSIONS
+#include "Vc/vector.h"
+#include <Vc/double_v>
+typedef Vc::double_v vd;
+#endif
 
 
-#ifndef VEC_EXTENSIONS
+#ifndef VEC_EXTENSIONS 
 //_____________________________________________________________________________                                                                                                     
 void TGeoCone_v::Contains_v(const StructOfCoord  & pointi, Bool_t * isin, Int_t np) const
 {
@@ -17,10 +23,54 @@ void TGeoCone_v::Contains_v(const StructOfCoord  & pointi, Bool_t * isin, Int_t 
 //_____________________________________________________________________________                                                                                                     
 void TGeoCone_v::Contains_v(const StructOfCoord  & pointi, Bool_t * isin, Int_t np) const
 {
-  for(unsigned int i = 0; i < np; i++)
+    static vd vfDz(fDz);
+    static vd vfRmin1(fRmin1);
+    static vd vfRmin2(fRmin2);
+    static vd vfRmax1(fRmax1);
+    static vd vfRmax2(fRmax2);
+    
+    Vc::double_m particleinside;
+    std::cout<<"fabio: "<<Vc::double_v::Size<<std::endl;
+    
+    int tailsize =np % Vc::double_v::Size;
+    for(unsigned int i = 0; i < np-tailsize; i+= Vc::double_v::Size)
     {
-      Double_t point[3] = {pointi.x[i], pointi.y[i], pointi.z[i]};
-      isin[i] = TGeoCone::Contains(point);
+        vd x(&pointi.x[i]); // will copy a certain number of x's into vc vector x;
+        vd y(&pointi.y[i]);
+        vd z(&pointi.z[i]);
+        
+        vd me=Vc::abs(z);
+        Vc::double_m c1 = (me > vfDz);
+        //  if( c1 ) continue;
+        
+        vd r2=x*x+y*y;
+        vd rl=0.5*(vfRmin2*(z+vfDz)+vfRmin1*(vfDz-z))/vfDz;
+        vd rh= 0.5*(vfRmax2*(z+vfDz)+vfRmax1*(vfDz-z))/vfDz;
+        Vc::double_m c2 = (r2<rl*rl);
+        // if( c2 ) continue;
+        
+        Vc::double_m c3 = (r2>rh*rh);
+        // if( c3 ) continue;
+        
+        //Vc::double_m particleinside;
+        particleinside = !c1 && !c2 && !c3;
+        for(unsigned int j=0;j<Vc::double_v::Size;++j) isin[i+j]= particleinside[j];
+    }
+    
+    // do the tail part for the moment, we just call the old static version
+    for(unsigned int i = 0; i < tailsize; ++i)
+    {
+        Double_t xx, yy, zz;
+        xx= pointi.x[np-tailsize+i];
+        yy= pointi.y[np-tailsize+i];
+        zz= pointi.z[np-tailsize+i];
+        
+        Double_t rr2 = xx*xx+yy*yy;
+        Double_t rrl = 0.5*(fRmin2*(zz+fDz)+fRmin1*(fDz-zz))/fDz;
+        Double_t rrh = 0.5*(fRmax2*(zz+fDz)+fRmax1*(fDz-zz))/fDz;
+        
+        
+        isin[i]=( (TMath::Abs(zz) <= fDz) & (rr2>=rrl*rrl) & (rr2<=rrh*rrh) );
     }
 }
 #endif
@@ -63,16 +113,90 @@ void TGeoCone_v::DistFromOutside_v(const StructOfCoord & pointi, const StructOfC
 
 }
 
-
+#ifndef VEC_EXTENSIONS
 //_____________________________________________________________________________                                                                                                     
 void TGeoConeSeg_v::Contains_v(const StructOfCoord  & pointi, Bool_t * isin, Int_t np) const
 {
-  for(unsigned int i = 0; i < np; i++)
+  std::cout<<"ma almeno qui? "<<std::endl;
+    
+    for(unsigned int i = 0; i < np; i++)
     {
+        
       Double_t point[3] = {pointi.x[i], pointi.y[i], pointi.z[i]};
       isin[i] = TGeoConeSeg::Contains(point);
     }
 }
+#else
+
+//_____________________________________________________________________________
+void TGeoConeSeg_v::Contains_v4( Vc::double_v const & x , Vc::double_v const & y, Vc::double_v const & z,  Vc::double_m & c1)
+{
+    vd vfDz(fDz);
+    vd vfRmin1(fRmin1);
+    vd vfRmin2(fRmin2);
+    vd vfRmax1(fRmax1);
+    vd vfRmax2(fRmax2);
+    
+    vd absZ=Vc::abs(z);
+    vd r2(x*x+y*y);
+    vd rl(0.5*(vfRmin2*(z+vfDz)+vfRmin1*(vfDz-z))/vfDz);
+    vd rh(0.5*(vfRmax2*(z+vfDz)+vfRmax1*(vfDz-z))/vfDz);
+    c1=((absZ<=vfDz) && (r2>=rl*rl) && (r2<=rh*rh));
+}
+
+//_____________________________________________________________________________
+void TGeoConeSeg_v::Contains_v(const StructOfCoord  & pointi, Bool_t * isin, Int_t np) const
+{
+    static vd vfPhi1(fPhi1);
+    static vd vfPhi2(fPhi2);
+    int vectorsize=Vc::double_v::Size;
+    int tailsize =np % vectorsize;
+    Vc::double_m particleinside;
+    
+    for(unsigned int i = 0; i < np-tailsize; i+= vectorsize)
+    {
+        vd x(&pointi.x[i]); // will copy a certain number of x's into vc vector x
+        vd y(&pointi.y[i]);
+        vd z(&pointi.z[i]);
+        vd dphi(vfPhi2 - vfPhi1);
+        vd phi = Vc::atan2(y, x) * 57.295780181884765625f; // 180/phi
+        //vd phi = Vc::atan2(y, x) * TMath::RadToDeg();
+        
+        phi(phi<0.f)+=360.f;
+        //if (phi < 0 ) phi+=360.;
+        vd ddp = phi-vfPhi1;
+        
+        ddp(ddp<0.f)+=360.f;
+        //if (ddp < 0) ddp+=360.;
+        Vc::double_m c1;
+        TGeoConeSeg_v::Contains_v4(x,y,z,c1);
+        
+        Vc::double_m c2 = (dphi>=360.);
+        Vc::double_m c3 = (ddp>dphi) ;
+        particleinside = c1 && (c2||(!c2 && !c3));
+        //particleinside = !c1;
+        for(unsigned int j=0; j<vectorsize; ++j)
+            isin[i+j] = (bool) particleinside[j];
+    }
+    // do the tail part for the moment, we just call the old static version
+    for(unsigned int i = 0; i < tailsize; ++i)
+    {
+        Double_t point[3];
+        
+        point[0]= pointi.x[np-tailsize+i];
+        point[1]= pointi.y[np-tailsize+i];
+        point[2]= pointi.z[np-tailsize+i];
+        
+        Double_t t_dphi = fPhi2 - fPhi1;
+        Double_t t_phi = TMath::ATan2(point[1], point[0]) * TMath::RadToDeg();
+        if (t_phi < 0 ) t_phi+=360.;
+        Double_t t_ddp = t_phi-fPhi1;
+        if (t_ddp < 0) t_ddp+=360.;
+        isin[i]=( (TGeoCone::Contains(point) & (t_ddp<=t_dphi)) | t_dphi>=360.);
+        
+    }
+}
+#endif
 
 //_____________________________________________________________________________
 
