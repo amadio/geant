d75824db65b50de49f05a2299be1c05195e63fd6
Andrei Gheata <Andrei.Gheata@cern.ch>
2013-09-24 17:35:46 +0200

    new files needed for the new version

---
+++ vecprot_v2/inc/globals.h
@@ -0,0 +1,11 @@
+#ifndef PROPAGATOR_GLOBALS
+#define PROPAGATOR_GLOBALS
+
+#include "GeantPropagator.h"
+
+R__EXTERN GeantPropagator   *gPropagator;     // propagator class
+
+// types
+enum Species_t {kHadron, kLepton};
+
+#endif
---
+++ vecprot_v2/inc/physical_constants.h
@@ -0,0 +1,10 @@
+
+static const  Double_t  PI  = 3.14159265358979323846;
+
+// Units
+static const  Double_t  MeV  = 1.0; 
+static const  Double_t  keV = 0.001 * MeV; 
+static const  Double_t   eV = 0.001 * keV; 
+
+// Physical constants 
+static const  Double_t  electron_mass_c2 = 0.510998910 * MeV;
