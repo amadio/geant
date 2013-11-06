b1fbeccfeac3855657a6784376c309af50f893bd
Federico Carminati <federico.carminati@cern.ch>
2013-11-06 17:32:30 +0100

    Timing routine

---
+++ tabxsec/src/TimingInfo.cxx
@@ -0,0 +1,45 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TPartIndex.h"
+
+static TFile *f;
+static TTree *t;
+
+void TimingInfo(Float_t cpu,Int_t z,Int_t pdg,Int_t proc,Float_t en, Int_t np)
+{
+  static Float_t scpu;
+  static Int_t sz;
+  static Int_t spcode;
+  static Int_t sreac;
+  static Float_t sen;
+  static Int_t snp;
+  static Bool_t first=kTRUE;
+  
+  if(first) {
+    f = new TFile("timing.root","recreate");
+    TPartIndex::I()->Write();
+    t = new TTree("G4time","G4 timings");
+    t->Branch("cpu",&scpu,"cpu/F");
+    t->Branch("z",&sz,"z/I");
+    t->Branch("part",&spcode,"part/I");
+    t->Branch("process",&sreac,"process/I");
+    t->Branch("energy",&sen,"energy/F");
+    t->Branch("nprod",&snp,"nprod/I");
+    first = kFALSE;
+  }
+  //create the file, the Tree and a few branches
+  scpu = cpu;
+  sz = z;
+  spcode = TPartIndex::I()->PartIndex(pdg);
+  sreac = TPartIndex::I()->ProcIndex(proc);
+  sen = en;
+  snp = np;
+  t->Fill();
+}
+
+void CloseTiming()
+{
+  t->Write();
+  f->Write();
+  f->Close();
+}
