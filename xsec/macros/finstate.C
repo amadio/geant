9da7ba7baaddc1436a58f4f5fed5be6a0128fb78
Federico Carminati <federico.carminati@cern.ch>
2013-11-13 14:05:53 +0100

    Ready for ACLiC + fixes

--- xsec/macros/finstate.C
+++ xsec/macros/finstate.C
@@ -1,3 +1,12 @@
+#include "TSystem.h"
+#include "TFile.h"
+#include "TProfile.h"
+#include "TMath.h"
+#include "TCanvas.h"
+
+#include "TEXsec.h"
+#include "TEFstate.h"
+
 void finstate(const char *proc="inElastic", const char *part="proton", Int_t elemin=1, Int_t elemax=92)
 {
   gSystem->Load("libXsec");
@@ -5,8 +14,8 @@ void finstate(const char *proc="inElastic", const char *part="proton", Int_t ele
   TEXsec *s = (TEXsec *) fx->Get("O");
   TFile *ff = new TFile("fstate.root","r");
 //  ff->ls();
-  iproc = TPartIndex::I()->ProcIndex(proc);
-  ipart = TPartIndex::I()->PartIndex(part);
+  Int_t iproc = TPartIndex::I()->ProcIndex(proc);
+  Int_t ipart = TPartIndex::I()->PartIndex(part);
 
   
   // Reaction list
@@ -27,40 +36,41 @@ void finstate(const char *proc="inElastic", const char *part="proton", Int_t ele
   }
   
   TProfile *hh[184];
+  memset(hh,0,184*sizeof(TProfile*));
   Int_t nh=0;
   
-  for(Int_t iele=elemin; iele<=elemax; ++iele) {
-    hh[iele-1]=0;
-    hh[92+iele-1]=0;
-    fs = (TEFstate *) ff->Get(TPartIndex::I()->EleSymb(iele));
+  for(Int_t iele=elemin-1; iele<elemax; ++iele) {
+    hh[iele]=0;
+    hh[92+iele]=0;
+    fs = (TEFstate *) ff->Get(TPartIndex::I()->EleSymb(iele+1));
     if(fs) {
       Int_t nsamp = fs->NEFstat();
       TString title = TString(TPartIndex::I()->PartName(ipart))
       +TString(" ")+ TString(TPartIndex::I()->ProcName(iproc))+TString(" on ")+TString(TPartIndex::I()->EleSymb(iele));
       TString name = TString(TPartIndex::I()->EleSymb(iele));
-      hh[iele-1] = new TProfile(name+"-mult", title,
+      hh[iele] = new TProfile(name+"-mult", title,
                               100,TMath::Log10(TPartIndex::I()->Emin()),
                               TMath::Log10(TPartIndex::I()->Emax()));
-      hh[92+iele-1] = new TProfile(name+"-pt", title,
+      hh[92+iele] = new TProfile(name+"-pt", title,
                                 100,TMath::Log10(TPartIndex::I()->Emin()),
                                 TMath::Log10(TPartIndex::I()->Emax()));
-      hh[iele-1]->GetXaxis()->SetTitle("Log10(GeV)");
-      hh[iele-1]->GetYaxis()->SetTitle("n particles");
-      hh[92+iele-1]->GetXaxis()->SetTitle("Log10(GeV)");
-      hh[92+iele-1]->GetYaxis()->SetTitle("pt");
+      hh[iele]->GetXaxis()->SetTitle("Log10(GeV)");
+      hh[iele]->GetYaxis()->SetTitle("n particles");
+      hh[92+iele]->GetXaxis()->SetTitle("Log10(GeV)");
+      hh[92+iele]->GetYaxis()->SetTitle("pt");
       Float_t kerma=0;
       Int_t npart=0;
-      Int_t *pid=0;
-      Float_t *mom=0;
+      const Int_t *pid=0;
+      const Float_t *mom=0;
       // Test of energy conservation in inelastic
-      Double_t *egrid = TPartIndex::I()->EGrid();
+      const Double_t *egrid = TPartIndex::I()->EGrid();
       for(Int_t ien=0; ien<TPartIndex::I()->NEbins(); ++ien) {
         for(Int_t is=0; is<nsamp; ++is) {
           Int_t isurv = fs->GetReac(ipart, egrid[ien], iproc, is, kerma, npart, pid, mom);
-          hh[iele-1]->Fill(TMath::Log10(egrid[ien]),npart);
+          hh[iele]->Fill(TMath::Log10(egrid[ien]),npart);
 //          printf("Here %d %p\n",npart,mom);
           if(npart)
-            hh[92+iele-1]->Fill(TMath::Log10(egrid[ien]),TMath::Sqrt(mom[3*is]*mom[3*is]+mom[3*is+1]*mom[3*is+1]));
+            hh[92+iele]->Fill(TMath::Log10(egrid[ien]),TMath::Sqrt(mom[3*is]*mom[3*is]+mom[3*is+1]*mom[3*is+1]));
           if(is<1000) {
 //            printf("en %10.3e #%d(%d) ",egrid[ien], npart,isurv);
 //            for(Int_t l=0; l<npart; ++l) if(pid[l]<1000) printf("%s ",TPartIndex::I()->PartName(pid[l]));
@@ -71,14 +81,14 @@ void finstate(const char *proc="inElastic", const char *part="proton", Int_t ele
       }
     }
   }
-  for(Int_t iele=1; iele<=92; ++iele) {
-    if(hh[iele-1]) {
+  for(Int_t iele=elemin-1; iele<elemax; ++iele) {
+    if(hh[iele]) {
       TCanvas *c = new TCanvas();
-      hh[iele-1]->Draw();
+      hh[iele]->Draw();
     }
-    if(hh[92+iele-1]) {
+    if(hh[92+iele]) {
       TCanvas *c = new TCanvas();
-      hh[92+iele-1]->Draw();
+      hh[92+iele]->Draw();
     }
   }
 }
