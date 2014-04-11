7f82873c17517d40f3238a7a1aa6800fc64a5cde
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-04-11 09:11:13 +0200

    addapted run.C for my purposes

--- vecprot_v2/run.C
+++ vecprot_v2/run.C
@@ -1,30 +1,31 @@
 void run(Int_t nthreads=1, Bool_t graphics=kFALSE, 
 //         const char *geomfile="gexam1.root")
 //         const char *geomfile="http://root.cern.ch/files/cms.root")
-	 const char *geomfile="prototype1geom.root")
+	 const char *geomfile="Ex03.root")
 {
    gSystem->Load("libPhysics.so");
    gSystem->Load("libHist.so");
    gSystem->Load("libThread.so");
    gSystem->Load("libGeom.so");
    gSystem->Load("libVMC.so");
-   gSystem->Load("libXsec.so");
+   gSystem->Load("../lib/libGeant_v.so");
+   gSystem->Load("../lib/libXsec.so");
 
    Int_t ntotal   = 100;  // Number of events to be transported
    Int_t nbuffered  = 10;   // Number of buffered events
    TGeoManager::Import(geomfile);
 
-   gSystem->Load("../lib/libGeant_v.so");
+
    //   gSystem->Load("../lib/libGeant_v_vecGeom.so");
 
-   Int_t ntotal   = 1;  // Number of events to be transported
-   Int_t nbuffered  = 1;   // Number of buffered events
+   Int_t ntotal   = 100;  // Number of events to be transported
+   Int_t nbuffered  = 10;   // Number of buffered events
 
    
    GeantPropagator *prop = GeantPropagator::Instance(ntotal, nbuffered);
    WorkloadManager *wmgr = WorkloadManager::Instance(nthreads);
    wmgr->SetNminThreshold(5*nthreads);
-   prop->fNaverage = 1;   // Average number of tracks per event
+   prop->fNaverage = 100;   // Average number of tracks per event
 
    prop->fNperBasket = 8;
    prop->fEmin = 1.E-5; // [10KeV]
