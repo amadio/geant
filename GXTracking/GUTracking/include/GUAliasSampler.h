6155abe9cabd0264108a3e1bda1629d4c7eb2190
John Apostolakis <john.apostolakis@cern.ch>
2014-10-22 18:02:19 -0500

    Further prototyping for Compton and Alias.

--- GXTracking/GUTracking/include/GUAliasSampler.h
+++ GXTracking/GUTracking/include/GUAliasSampler.h
@@ -8,78 +8,172 @@
 //
 //  Desing choice: One Sampler per element 
 //                    (potentially later per composit material?)
+//
+//  First version assumes linear 'x' integrand
+//   TODO: a measure will be required for the integration for log, theta etc.
 // 
 //  Based on first alias sampler by Soon Y. Jun - July 2014
 
 class GUAliasSampler
 {
   public: 
-  	GUAliasSampler(int Zelement);  // One table per element !!
+  	GUAliasSampler(int    Zelement, 
+  		           double incomingMin, 
+  		           double incomingMax,
+  		           int    numEntriesIncoming,  // for 'energy' (or log) of projectile
+  		           int    numEntriesSampled   
+  		           );  
 
     template<class Backend>
     typename Backend::Double_t
-    SampleX( typename Backend::Int_t irow,   // ~ sampled value 
-		     typename Backend::Int_t icol,   // ~ input Energy
-			 typename Backend::Double_t dy, 
-			 typename Backend::Double_t tt );
-    BuildTable();  
+    Sample( typename Backend::Double_t energyIn, 
+          	typename Backend::Double_t deltaY    ) const;  
+
+    void BuildRow( int numVal, double* differentialXSection, double xMin, double xMax );
       // Builds all our table - must be called during initialisation 
 
 private:
 // Implementation methods: 
 	BuildPdfTable(); 
-	BuildAlisTable(); 
+	BuildAliasTable(); 
+
+    template<class Backend>
+    typename Backend::Double_t
+    Sample( typename Backend::Index_t  irow,   // ~ sampled value 
+		    typename Backend::Index_t  icol,   // ~ input Energy
+			typename Backend::Double_t binSize,
+			typename Backend::Double_t remainderX  //  in sampled variable
+          ) const;
 
 private:
-    int      fZelement; 
+    const int      fZelement; 
+
+    const double   fIncomingMin; // Min of Incoming - e.g. e_Kinetic or Log(E_kinetic)
+    const double   fIncomingMax; // Max
+    const int      fInNumEntries;
+    const double   fInverseBinIncoming;
+    // const double   fInBinSize; 
+
+    //  For the sampled variable 
+    const int      fSampledNumEntries;   //  Old name fNcol  (number of Columns)
+    const double   fSampledBinSize; 
+    const double   fInverseBinSampled; 
+    // Values needed per bin 
+    //  -- Initialised how ??
+    double*  fSampledMin; // Minimum value of 'x' sampled variable
+    double*  fSampledMax; // Maximum value of 'x' sampled variable
+
+    // Effective 2-dimensional arrays - size is fInNumEntries * fSampledNumEntries
 	double * fpdfX; // Original distribution
+
 	double * fpdfY; // Non-alias probability 
-	int    * fpdfA; // Alias table
+	int    * fpdfA; // Alias table           -- could become Index_t ?
 };
 
+template<class Backend>
+GUAliasSampler::
+GUAliasSampler(int    Zelement, 
+  		           double incomingMin, 
+  		           double incomingMax,
+  		           int    numEntriesIncoming,  // for 'energy' (or log) of projectile
+  		           int    numEntriesSampled   
+  		           )  
+  :
+    fIncomingMin( incomingMin ),
+    fIncomingMax( incomingMax ),
+    fInNumEntries(numEntriesIncoming), 
+    fInverseBinIncoming( numEntriesIncoming / incomingMax-incomingMin)
+	fSampledNumEntries( numEntriesSampled ),
+	fInverseBinSampled( 1.0 / (numEntriesSampled-1) )  // Careful - convention build / use table!
+{
+}
 
 template<class Backend>
 typename Backend::Double_t
-GUAliasSampler::SampleX( typename Backend::Int_t irow, 
-	   typename Backend::Int_t icol, 
-	   typename Backend::Double_t dy, 
-	   typename Backend::Double_t tt )
+GUAliasSampler::
+Sample( typename Backend::Double_t energyIn ) const  
+{
+  typedef typename Backend::Double_t Double_t;
+  typedef typename Backend::Bool_t   Bool_t;
+  typedef typename Backend::Int_t    Int_t;	
+
+  Int_t     irow, icol;
+  Double_t  fraction;
+
+  GetBin(energyIn, &irow, &icol, &fraction);
+
+  // MinY = kineticEnergy/(1+2.0*kineticEnergy/electron_mass_c2);
+  // MaxY = kineticEnergy;
+  // DeltaY = fMaxY - fMinY;
+
+  Double_t deltaY= fSampledMax[irow] - fSampledMin[irow]; 
+
+  Double_t x = SampleX( irow, icol, fraction);
+
+  return x; 
+}
+
+template<class Backend>
+FQUALIFIER void
+GUAliasSampler::GetBin(typename Backend::Double_t  kineticEnergy,
+				       typename Backend::Int_t    &irow, 
+				       typename Backend::Int_t    &icol,
+				       typename Backend::Double_t &t) 
+{
+  irow = floor((kineticEnergy - fIncomingMin)*fInverseBinIncoming);
+  G4double r1 = (fSampledNumEntries-1)*GPUniformRand(fDevState, fThreadId);
+  icol = floor(r1);
+  t = r1 - 1.0*icol;
+}
+
+template<class Backend>
+typename Backend::Double_t
+GUAliasSampler::SampleX( typename Backend::Index_t  irow, // Energy bin of incoming particle
+				  	     typename Backend::Index_t  icol, // Bin index for this 'x' sample
+					     typename Backend::Double_t rangeSampled, 
+					     typename Backend::Double_t fraction )
 {
 	typedef typename Backend::Double_t Double_t;
-	typedef typename Backend::Bool_t Bool_t;
-	typedef typename Backend::Int_t Int_t;
-	
+	typedef typename Backend::Bool_t   Bool_t;
+	typedef typename Backend::Index_t  Index_t;
+	typedef typename Backend::Int_t    Int_t;
 	
 	Double_t r1 = UniformRand(  );
 	
 	Double_t xd, xu;
-	Double_t dx = dy/(fNcol-1); // dy*(fNcolMinusInverse)
+	Double_t binSampled = rangeSampled * fInverseBinSampled; 
+              // Was rangeSampled /(fSampledNumEntries-1);
 
-	Double_t thresh;
-	Double_t Aval;
-	// fill thresh from table
-	Int_t index = irow*fNcol  + icol; 
+	Double_t probNA;   // Non-alias probability
+	Double_t aliasInd; //  This is really an integer -- could be Index_t !?  
+	// fill probNA from table
+	Index_t index = irow*fSampledNumEntries  + icol; 
 	
-	// should investigate here whether gather is supported in Vc
+	// Gather
 	for( int i=0;i < Double_t::Size; ++i )
 	{
-		thresh[i]=fPDFY[ index[i] ];
-	    Aval[i]=fPDFA[ index[i] ];
+		int iEntry= ConverfractionoInteger(index[i]);
+		probNA[i]=    fPDFY[ iEntry ]; // index[i] ];
+	    aliasInd[i]=  fPDFA[ iEntry ]; // index[i] ];
 	}
-	
-	Bool_t condition = r1 <= thresh;
+	// should investigate here whether gather is supported in Vc
+
+	Bool_t condition = r1 <= probNA;
 	
 	// if branch
 	
-	MaskedAssign( condition, dx*icol ,xd );
-	MaskedAssign( condition, dx*(icol+1) ,xu );
+	MaskedAssign( condition, icol*binSampled ,     xd );   // Stores into xd
+	MaskedAssign( condition, (icol+1)*binSampled , xu );   //        into xu
 
 	// else branch
 	
-	MaskedAssign( !condition, dx*Aval ,xd );
-    MaskedAssign( !condition, dx*(Aval + 1) ,xu );
+	MaskedAssign( !condition,  aliasInd*binSampled    , xd );
+    MaskedAssign( !condition, (aliasInd+1)*binSampled , xu );
 
-    Double_t x = ( 1 - tt)*xd + tt*xu;
+    Double_t x = (1 - fraction) * xd + fraction * xu;
 
     return x;
 }
+
+
+
