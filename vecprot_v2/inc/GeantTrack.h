e624a95cebe7f64115c5cd2c4003e8b27e0316b3
Philippe Canal <pcanal@fnal.gov>
2014-10-29 15:38:04 -0500

    GEANT-103 remove TObject inheritance

--- vecprot_v2/inc/GeantTrack.h
+++ vecprot_v2/inc/GeantTrack.h
@@ -40,7 +40,7 @@ enum Species_t {kHadron, kLepton};
 class GeantTrack_v;
 
 //______________________________________________________________________________
-class GeantTrack : public TObject {
+class GeantTrack {
 public:
    Int_t    fEvent;     // event number
    Int_t    fEvslot;    // event slot
@@ -124,7 +124,7 @@ public:
    Species_t          Species() const {return fSpecies;}
    TrackStatus_t      Status() const  {return fStatus;}
    
-   virtual void       Clear(Option_t *option="");
+   void               Clear(Option_t *option="");
    Double_t           X() const {return fXpos;}
    Double_t           Y() const {return fYpos;}
    Double_t           Z() const {return fZpos;}
@@ -157,7 +157,7 @@ public:
    void               SetPath(VolumePath_t const * const path);
    void               SetNextPath(VolumePath_t const * const path);
    
-   ClassDef(GeantTrack, 1)      // The track
+   ClassDefNV(GeantTrack, 1)      // The track
 };
 
 //______________________________________________________________________________
@@ -220,7 +220,7 @@ public:
 
 private:
    GeantTrack_v(const GeantTrack_v &track_v); // not allowed
-   GeantTrack_v &operator=(const GeantTrack_v &track_v); // not allowed
+   GeantTrack_v &operator=(const vecprot_v2/inc/GeantTrack.hGeantTrack_v &track_v); // not allowed
 
 public:   
    GeantTrack_v();
@@ -257,7 +257,7 @@ public:
    Int_t     SortByStatus(TrackStatus_t status);
    Int_t     RemoveByStatus(TrackStatus_t status, GeantTrack_v &output);
    Bool_t    IsSelected(Int_t i)  {return fSelected.TestBitNumber(i);}
-   virtual void      Clear(Option_t *option="");
+   void      Clear(Option_t *option="");
    Int_t     Compact(GeantTrack_v *moveto=0);
    Bool_t    Contains(Int_t evstart, Int_t nevents=1) const;
    void      ClearSelection()     {fSelected.ResetAllBits();}
@@ -314,7 +314,7 @@ public:
       return (num+ALIGN_PADDING-remainder);
    }  
 
-   ClassDef(GeantTrack_v, 1)      // SOA for GeantTrack class           
+   ClassDefNV(GeantTrack_v, 1)      // SOA for GeantTrack class
 };
 
 #endif
