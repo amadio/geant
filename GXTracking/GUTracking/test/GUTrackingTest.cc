1dea4efccb5e487e47d1f36c870c5db93a5ea7b6
Soon Yung Jun <syjun@mic.fnal.gov>
2014-11-14 10:19:45 -0600

    add CMakeLists and make compilable under GeantV

---
+++ GXTracking/GUTracking/test/GUTrackingTest.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "GUXSectionKleinNishina.h"
+#include "GUAliasSampler.h"
+#include "GURandom.h"
+#include "GUTrack.h"
+#include "GUComptonKleinNishina.h"
+
+using namespace vecgeom;
+
+int main(int argc, char* argv[]) {
+  std::cout << "GUTrackingTest" << std::endl;
+  //put GUComptonProcess here which will call
+  //GUComptonKleinNishina::Interact(...)
+  return 0;
+}
