086d7c67b396cbc410a77ec487de0841a22710ed
Oksana Shadura <oksana.shadura@cern.ch>
2015-01-09 20:01:34 +0100

    Clang-formatting according LLVM format and doxygen comments for physical_constants.h & priority_queue.h

--- vecprot_v2/inc/physical_constants.h
+++ vecprot_v2/inc/physical_constants.h
@@ -1,10 +1,21 @@
+//===--- physical_constants.h - Geant-V -------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file physical_constants.h
+ * @brief Definition of physical constants used in Geant-V prototype 
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
 
-static const  Double_t  PI  = 3.14159265358979323846;
+static const Double_t PI = 3.14159265358979323846;
 
 // Units
-static const  Double_t  MeV  = 1.0; 
-static const  Double_t  keV = 0.001 * MeV; 
-static const  Double_t   eV = 0.001 * keV; 
+static const Double_t MeV = 1.0;
+static const Double_t keV = 0.001 * MeV;
+static const Double_t eV = 0.001 * keV;
 
-// Physical constants 
-static const  Double_t  electron_mass_c2 = 0.510998910 * MeV;
+// Physical constants
+static const Double_t electron_mass_c2 = 0.510998910 * MeV;
