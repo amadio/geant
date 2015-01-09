af291af6267f739a30f9bee4f7949bc72c9b0e53
Oksana Shadura <oksana.shadura@cern.ch>
2015-01-09 19:55:37 +0100

    Clang-formating according LLVM format and doxygen comments for PrimaryGenerator.h & TaskBroker.h

--- vecprot_v2/inc/PrimaryGenerator.h
+++ vecprot_v2/inc/PrimaryGenerator.h
@@ -1,4 +1,14 @@
+//===--- PrimaryGenerator.h - Geant-V ---------------------------*- C++ -*-===//
 //
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file PrimaryGenerator.h
+ * @brief Implementation of primary generators for Geant-V prototype 
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
 
 #ifndef GEANTV_PrimaryGenerator_h
 #define GEANTV_PrimaryGenerator_h
@@ -6,16 +16,29 @@
 class TParticlePDG;
 class GeantTrack;
 
-class PrimaryGenerator : public TNamed{
-    
+/**
+ * @brief Class of primary generators
+ */
+class PrimaryGenerator : public TNamed {
+
 public:
-    
-    // set one GeantTrack primary track properties
-    virtual void InitPrimaryGenerator() = 0;
-    virtual Int_t NextEvent() = 0;
-    virtual void GetTrack(Int_t n, GeantTrack &gtrack) = 0;
 
+ /**
+  * @brief Pure virtual function of initialization of primary generator
+  * @details Set one GeantTrack primary track properties
+  */
+  virtual void InitPrimaryGenerator() = 0;
+
+  /** @brief  Pure virtual function that produce next event */
+  virtual Int_t NextEvent() = 0;
+
+ /**
+  * @brief Pure virtual function that returns track
+  * 
+  * @param n ?????
+  * @param gtrack track
+  */
+  virtual void GetTrack(Int_t n, GeantTrack &gtrack) = 0;
 };
-
 
 #endif
