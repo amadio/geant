10967a5bf7693c96d06ec2bbcaa7371c4a153af6
Oksana Shadura <oksana.shadura@cern.ch>
2015-01-09 19:45:54 +0100

    Clang-formatting and doxygen comments for GeantRqueue.h

--- vecprot_v2/inc/GeantRqueue.h
+++ vecprot_v2/inc/GeantRqueue.h
@@ -1,3 +1,15 @@
+//===--- GeantRqueue.h - Geant-V --------------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file GeantRqueue.h
+ * @brief Definition of general queue for runnables.
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
+
 #ifndef GEANT_RQUEUE
 #define GEANT_RQUEUE
 
@@ -13,35 +25,47 @@
 #include "GeantVRunnable.h"
 #endif
 
-//______________________________________________________________________________
-//   GeantRqueue - A general queue for runnables. 
-//______________________________________________________________________________
-
-//______________________________________________________________________________
-class GeantRqueue : public TObject
-{
+/** @brief GeantRqueue - A general queue for runnables. */
+class GeantRqueue : public TObject {
 private:
-   std::deque<GeantVRunnable*> fQueue;      // Queue of runnables
-   mutable TMutex              fMutex;      // Mutex for the queue
-   TCondition                  fCondition;  // Condition to take next runnable
-   Int_t                       fMax;        // Maximum allowed size
-   Int_t                       fN;          // Numer of objects in the queue
-   Int_t                       fNp;         // Number of prioritized objects
+  std::deque<GeantVRunnable *> fQueue; /** Queue of runnables */
+  mutable TMutex fMutex;               /** Mutex for the queue */
+  TCondition fCondition;               /** Condition to take next runnable */
+  Int_t fMax;                          /** Maximum allowed size */
+  Int_t fN;                            /** Numer of objects in the queue */
+  Int_t fNp;                           /** Number of prioritized objects */
 
 public:
 
-   // Blocking push. The caller is quarantined if the queue is full
-   void            Push(GeantVRunnable *data, Bool_t priority=kFALSE);   
+  /**
+   * @brief Push function
+   * @details Blocking push. The caller is quarantined if the queue is full
+   * 
+   * @param data GeantVRunnable data object
+   * @param priority Priority parameter (by default false)
+   */
+  void Push(GeantVRunnable *data, Bool_t priority = kFALSE);
+  
+  /**
+   * @brief Function that check possibility of push
+   * @details Try push. If the queue is full return false and the data object is not injected
+   * 
+   * @param data GeantVRunnable data object
+   * @param priority Priority parameter (by default false)
+   * 
+   */
+  Bool_t TryPush(GeantVRunnable *data, Bool_t priority = kFALSE);
+  
+  /**
+   * @brief Function that run next runnable
+   * @details Run next runnable in the queue, possibly. Blocking if the queue is empty.
+   */
+  GeantVRunnable *RunNext();
+  
+  /**
+   * @brief Function that try to run next runnable
+   * @details Try to run next runnable in the queue. Non-blocking even if the queue is empty
+   */
+  GeantVRunnable *TryRunNext();
 
-   // Try push. If the queue is full return false and the data obkect is not injected
-   Bool_t          TryPush(GeantVRunnable *data, Bool_t priority=kFALSE);
-   
-   // Run next runnable in the queue, possibly. Blocking if the queue is empty.
-   GeantVRunnable *RunNext();
-
-   // Try to run next runnable in the queue. Non-blocking even if the queue is empty
-   GeantVRunnable *TryRunNext();
-   
-   // Extract objects from the queue
-   
-   
+// Extract objects from the queue
