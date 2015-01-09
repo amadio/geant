6115467b70859203332cbf9120288cc607d72c55
Oksana Shadura <oksana.shadura@cern.ch>
2015-01-09 19:46:33 +0100

    Clang-formatting and doxygen comments for GeantScheduler.h

--- vecprot_v2/inc/GeantScheduler.h
+++ vecprot_v2/inc/GeantScheduler.h
@@ -1,3 +1,17 @@
+//===--- GeantScheduler.h - Geant-V -----------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file GeantScheduler.h
+ * @brief Implementation of dispatcher running in a single thread. Collects tracks
+ * from all threads via an input queue and fills baskets corresponding to each
+ * volume, which are then injected in the main work queue.
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
+
 #ifndef GEANT_SCHEDULER
 #define GEANT_SCHEDULER
 
@@ -7,72 +21,165 @@
 
 #ifdef __STAT_DEBUG
 #include "GeantTrackStat.h"
-#endif   
+#endif
 
 #if __cplusplus >= 201103L
 #include <atomic>
 #endif
 
 #include "TMutex.h"
-//==============================================================================
-// GeantScheduler - dispatcher running in a single thread. Collects tracks
-// from all threads via an input queue and fills baskets corresponding to each
-// volume, which are then injected in the main work queue.
-//==============================================================================
 
 class concurrent_queue;
 class GeantTrack;
 class GeantBasket;
 class GeantBasketMgr;
 
-//______________________________________________________________________________
+/**
+ * @brief Class GeantScheduler
+ * @details Dispatcher running in a single thread. Collects tracks
+ * from all threads via an input queue and fills baskets corresponding to each
+ * volume, which are then injected in the main work queue.
+ * 
+ */
 class GeantScheduler : public TObject {
 protected:
-   Int_t                fNvolumes;            // Number of active volumes in the geometry
-   Int_t                fNpriority;           // Number of priority baskets held
-   GeantBasketMgr     **fBasketMgr;           // Array of basket managers
-   GeantBasketMgr      *fGarbageCollector;    // Garbage collector manager
+  Int_t fNvolumes;                   /** Number of active volumes in the geometry */
+  Int_t fNpriority;                  /** Number of priority baskets held */
+  GeantBasketMgr **fBasketMgr;       /** Array of basket managers */
+  GeantBasketMgr *fGarbageCollector; /** Garbage collector manager */
 #if __cplusplus >= 201103L
-   std::atomic_int     *fNtracks;             //[fNvolumes] Number of tracks per volume
+  std::atomic_int *fNtracks; /**[fNvolumes] Number of tracks per volume */
 #endif
-   Int_t                fPriorityRange[2];    // Prioritized events
+  Int_t fPriorityRange[2]; /** Prioritized events */
 #ifdef __STAT_DEBUG
-   GeantTrackStat       fPStat;  //! Statistics for the pending tracks
-   GeantTrackStat       fQStat;  //! Statistics for the queued tracks
-   GeantTrackStat       fTStat;  //! Statistics for the transported tracks
-#endif   
+  GeantTrackStat fPStat; /** Statistics for the pending tracks */
+  GeantTrackStat fQStat; /** Statistics for the queued tracks */
+  GeantTrackStat fTStat; /** Statistics for the transported tracks */
+#endif
 
 private:
-   GeantScheduler(const GeantScheduler&);
-   GeantScheduler& operator=(const GeantScheduler&);
-  
+
+  /** @brief GeantScheduler constructor */
+  GeantScheduler(const GeantScheduler &);
+
+  /** @brief Function operator= */
+  GeantScheduler &operator=(const GeantScheduler &);
+
 public:
-   GeantScheduler();
-   virtual ~GeantScheduler();
-   Int_t                AddTrack(GeantTrack &track);
-   Int_t                AddTracks(GeantBasket *output, Int_t &ntot, Int_t &nnew, Int_t &nkilled);
-   void                 AdjustBasketSize();
-   void                 CreateBaskets();
-   void                 CleanBaskets();
-   Int_t                CollectPrioritizedTracks();
-   GeantBasketMgr     **GetBasketManagers() const {return fBasketMgr;}
-   GeantBasketMgr      *GetGarbageCollector() const {return fGarbageCollector;}
+
+  /** GeantScheduler constructor */
+  GeantScheduler();
+
+  /** GeantScheduler destructor */
+  virtual ~GeantScheduler();
+
+  /**
+   * @brief Function that add track
+   * 
+   * @param track Track that should be added
+   */
+  Int_t AddTrack(GeantTrack &track);
+
+  /**
+   * @brief Function for addition tracks
+   * 
+   * @param output Output GeantBasket object
+   * @param ntot Total number of tracks
+   * @param nnew Number of new tracks
+   * @param nkilled Number of killed tracks
+   */
+  Int_t AddTracks(GeantBasket *output, Int_t &ntot, Int_t &nnew, Int_t &nkilled);
+
+  /** @brief Function that adjust basket size */
+  void AdjustBasketSize();
+
+  /** @brief Function that create baskets */
+  void CreateBaskets();
+
+  /** @brief Function that clean baskets */
+  void CleanBaskets();
+
+  /** @brief Function that provides collection prioritized tracks*/
+  Int_t CollectPrioritizedTracks();
+
+  /**
+   * @brief Function that return basket managers
+   * return Array of basket managers
+   * */
+  GeantBasketMgr **GetBasketManagers() const { return fBasketMgr; }
+
+  /**
+   * @brief Function that return garbage collector
+   * @return Garbage collector manager
+   */
+  GeantBasketMgr *GetGarbageCollector() const { return fGarbageCollector; }
+
 #if __cplusplus >= 201103L
-   Int_t                GetNtracks(Int_t ib) {return fNtracks[ib].load();}
+
+  /**
+   * @brief Function that return N tracks
+   * 
+   * @param ib ?????
+   * @return return number of tracks per volume
+   */
+  Int_t GetNtracks(Int_t ib) { return fNtracks[ib].load(); }
 #endif
-   Int_t                GetNpriority() const {return fNpriority;}
-   Int_t                GetNvolumes() const  {return fNvolumes;}
-   void                 SetPriorityRange(Int_t min, Int_t max) {fPriorityRange[0]=min; fPriorityRange[1]=max;}
+
+  /**
+   * @brief Function that return N priority tracks
+   * @return Number of priority baskets held
+   */
+  Int_t GetNpriority() const { return fNpriority; }
+
+  /**
+   * @brief Function that return N volumes
+   * @return Number of active volumes in the geometry
+   */
+  Int_t GetNvolumes() const { return fNvolumes; }
+
+  /**
+   * @brief Function that return of range of prioritized events
+   * 
+   * @param min Minimum value of range
+   * @param max Maximum value of range
+   */
+  void SetPriorityRange(Int_t min, Int_t max) {
+    fPriorityRange[0] = min;
+    fPriorityRange[1] = max;
+  }
 #ifdef __STAT_DEBUG
-   GeantTrackStat      &GetPendingStat() {return fPStat;}
-   GeantTrackStat      &GetQueuedStat()  {return fQStat;}
-   GeantTrackStat      &GetTransportStat()  {return fTStat;}
-#endif   
-   Int_t                FlushPriorityBaskets();
-   Int_t                GarbageCollect();
-   void                 PrintSize() const;
-   size_t               Sizeof() const;
-      
-   ClassDef(GeantScheduler, 1)      // Main basket scheduler
-};   
+
+  /**
+   * @brief [Function that return statistic of pending tracks
+   * @return Statistics for the pending tracks
+   */
+  GeantTrackStat &GetPendingStat() { return fPStat; }
+
+  /**
+   * @brief Function that return statistic of queued tracks
+   * @return Statistics for the queued tracks
+   */
+  GeantTrackStat &GetQueuedStat() { return fQStat; }
+
+  /**
+   * @brief Function that return statistic of transported tracks
+   * @return Statistics for the transported tracks
+   */
+  GeantTrackStat &GetTransportStat() { return fTStat; }
+#endif
+
+  /** @brief Function that flush list of priority tracks */
+  Int_t FlushPriorityBaskets();
+
+  /** @brief Garbage collection function */
+  Int_t GarbageCollect();
+
+  /** @brief Function that print size */
+  void PrintSize() const;
+
+  /** @brief Function that returns size */
+  size_t Sizeof() const;
+
+  ClassDef(GeantScheduler, 1) // Main basket scheduler
+};
 #endif
