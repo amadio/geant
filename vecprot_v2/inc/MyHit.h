a21defcd7c215f6e321e0b17c7c684b352ae05f6
Oksana Shadura <oksana.shadura@cern.ch>
2015-01-09 19:54:02 +0100

    Clang-formating according LLVM format and doxygen comments for GeantHit.h

--- vecprot_v2/inc/MyHit.h
+++ vecprot_v2/inc/MyHit.h
@@ -1,3 +1,15 @@
+//===--- MyHit.h - Geant-V --------------------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file MyHit.h
+ * @brief Implementation of hits in user application for Geant-V prototype 
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
+
 #ifndef GEANT_MYHIT
 #define GEANT_MYHIT
 
@@ -5,23 +17,53 @@
 #include "Rtypes.h"
 #endif
 
-//______________________________________________________________________________
+/**
+ * @brief Class MyHit
+ * 
+ */
 class MyHit {
 public:
-   double fX;      // position
-   double fY;
-   double fZ;
-   double fEdep;     // energy loss
-   int    fVolId;  // volume Id
-   int    fDetId;  // replica (segmentation)
-   
-   MyHit() : fX(0), fY(0), fZ(0), fEdep(0), fVolId(0), fDetId(0) {}
-   MyHit(double x, double y, double z, double edep, int volid, int detid);
-   ~MyHit() {}
-   
-   void               Reset() {fX=0.; fY=0.; fZ=0.; fEdep=0.; fVolId=0; fDetId=0;}
-   void               AddHit();
-   
-   ClassDefNV(MyHit, 1)      // User hit
+  double fX; /** X position */
+  double fY; /** Y position */
+  double fZ; /** Z position */
+  double fEdep; /** Energy loss */
+  int fVolId;   /** Volume Id */
+  int fDetId;   /** Replica (segmentation) */
+
+  /** @brief MyHit constructor */
+  MyHit() : fX(0), fY(0), fZ(0), fEdep(0), fVolId(0), fDetId(0) {}
+  
+  /**
+   * @brief MyHit constructor
+   * 
+   * @param x X position 
+   * @param y Y position
+   * @param z Z position
+   * @param edep Energy loss
+   * @param volid Volume Id
+   * @param detid Replica (segmentation)
+   */
+  MyHit(double x, double y, double z, double edep, int volid, int detid);
+  
+  /** @brief MyHit destructor*/
+  ~MyHit() {}
+  
+  /**
+   * @brief Reset function 
+   * @details Set default values for MyHit object
+   */
+  void Reset() {
+    fX = 0.;
+    fY = 0.;
+    fZ = 0.;
+    fEdep = 0.;
+    fVolId = 0;
+    fDetId = 0;
+  }
+
+  /** @brief Function that add user hit */
+  void AddHit();
+
+  ClassDefNV(MyHit, 1) // User hit
 };
 #endif
