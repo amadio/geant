936f094a56154f0166eff0253982beb434d6cb22
John Apostolakis <john.apostolakis@cern.ch>
2015-03-24 20:54:47 +0100

    Added ability to set min/max energy(=momentum for photons)

--- GXTracking/GUTracking/test/GUModelBenchmark.cc
+++ GXTracking/GUTracking/test/GUModelBenchmark.cc
@@ -4,26 +4,49 @@
 
 using namespace vecphys;
 
+// These belong in a Units.h file
+constexpr double GeV = 1.0;
+constexpr double MeV = 0.001 * GeV;
+constexpr double KeV = 0.001 * MeV;
+
 int main(int argc, char* argv[]) {
 
   //default run
   int ntracks = 4992;
   int nrepetitions = 100;
-  constexpr double MeV = 0.001;
-  double energy = 0.5 * MeV;
+
+  // double minEnergy =  10.0 * KeV;
+  // double maxEnergy = 500.0 * MeV;
+
+  double minEnergy =  50.0 * KeV;
+  double maxEnergy =  minEnergy; 
   
   if(argc >= 2) ntracks =      atoi(argv[1]);
   if(argc >= 3) nrepetitions = atoi(argv[2]);
   if(argc >= 4) {
-     energy  =  atof(argv[2]);  
-     std::cout << " Main: argument energy = " << energy << std::endl;
+     minEnergy  =  atof(argv[3]);
+     std::cout << "  Min energy (GeV) = " << minEnergy << std::endl;
+
+     maxEnergy = minEnergy;  // Assume mono-energetic if no max is defined
+     if(argc >= 5) {
+        maxEnergy  =  atof(argv[4]);
+        std::cout << "  Max energy (GeV) = " << maxEnergy << std::endl;
+     } else {
+        std::cout << "  Mono-energetic> max energy (GeV) = " << maxEnergy << std::endl;
+     }
   } else {
-     std::cout << " Main: default  energy = " << energy << std::endl;
+     std::cout << " Using defaults:  min energy (GeV) = " << minEnergy << std::endl;
+     std::cout << "                  max energy (GeV) = " << maxEnergy << std::endl;
   }
 
   GUBenchmarker tester;
   tester.SetNTracks(ntracks);
   tester.SetRepetitions(nrepetitions);
+
+  // P = E for gammas (only)
+  tester.SetMaxP( minEnergy );
+  tester.SetMaxP( maxEnergy );
+  
   int status = tester.RunBenchmark();
 
   if(status==1) std::cout << "RunBenchmark Failed" << std::endl;
