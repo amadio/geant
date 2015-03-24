0c3984fdc007abf5399392ca71e59d57acfb7892
John Apostolakis <john.apostolakis@cern.ch>
2015-03-24 19:58:44 +0100

    Added ability to read energy as 3rd argument. (Not yet used.)

--- GXTracking/GUTracking/test/GUModelBenchmark.cc
+++ GXTracking/GUTracking/test/GUModelBenchmark.cc
@@ -9,9 +9,17 @@ int main(int argc, char* argv[]) {
   //default run
   int ntracks = 4992;
   int nrepetitions = 100;
-
-  if(argc >= 2) ntracks = atoi(argv[1]);
+  constexpr double MeV = 0.001;
+  double energy = 0.5 * MeV;
+  
+  if(argc >= 2) ntracks =      atoi(argv[1]);
   if(argc >= 3) nrepetitions = atoi(argv[2]);
+  if(argc >= 4) {
+     energy  =  atof(argv[2]);  
+     std::cout << " Main: argument energy = " << energy << std::endl;
+  } else {
+     std::cout << " Main: default  energy = " << energy << std::endl;
+  }
 
   GUBenchmarker tester;
   tester.SetNTracks(ntracks);
