c109ed2154650d150abad58968224fa374c2b668
John Apostolakis <john.apostolakis@cern.ch>
2015-03-24 19:58:01 +0100

    Added method to record values in histograms. Added TH1Fs.

--- GXTracking/GUTracking/inc/GUHistogram.h
+++ GXTracking/GUTracking/inc/GUHistogram.h
@@ -10,15 +10,26 @@ class GUHistogram
 {
 public:
  
-  GUHistogram(std::string fileName);
+  GUHistogram(std::string fileName, double maxEnergy );
   ~GUHistogram();
 
-  TH1F*  ftime;
-  TH1F*  fenergy;
-  TH1F*  fangle;
+  void RecordTime( double );
+  void RecordHistos( double EnPrimary,
+                     double EnFinalGamma,
+                     double angleGamma,    
+                     double EnElectron,
+                     double angleElectron);
 
 private:
-  void BookHistograms();
+  TH1F*  ftime;
+  TH1F*  fenergyPrimary;
+  TH1F*  fenergyGam;
+  TH1F*  fangleGam;
+  TH1F*  fenergyElec;
+  TH1F*  fangleElec;  
+  
+private:
+  void BookHistograms( double maxEnergy );
 
 private:
   TFile* fHistFile;
--- GXTracking/GUTracking/src/GUHistogram.cc
+++ GXTracking/GUTracking/src/GUHistogram.cc
@@ -2,10 +2,10 @@
 
 namespace vecphys {
 
-GUHistogram::GUHistogram(std::string fileName)
+GUHistogram::GUHistogram(std::string fileName, double maxE)
 {
   fHistFile = new TFile(fileName.c_str(),"RECREATE");
-  BookHistograms();
+  BookHistograms( maxE );
 }
 
 GUHistogram::~GUHistogram()
@@ -15,14 +15,36 @@ GUHistogram::~GUHistogram()
   fHistFile->Close();
 }
 
-void GUHistogram::BookHistograms()
+void GUHistogram::BookHistograms( double maxE )
 {
-  fHistFile->mkdir("Benchmark");
-  fHistFile->cd("Benchmark");
+  const char* dir= "Benchmark";
+  fHistFile->mkdir(dir); // ("Benchmark");
+  fHistFile->cd(dir);    // "Benchmark");
 
-  ftime   = new TH1F("ftime","ftime",100,0.,0.1);
-  fenergy = new TH1F("fenergy","fenergy",100,0.,1000.);
-  fangle  = new TH1F("fangle","fangle",100,0.,1.0);
+  ftime          = new TH1F("ftime",           "Elapsed time",     100, 0.,  0.001);
+  fenergyPrimary = new TH1F("Energy_primary",  "Energy Primary",   100, 0.,  1.1 * maxE);  
+  fenergyGam     = new TH1F("Energy_gammaOut", "Energy Gamma/Out", 100, 0.,  1.1 * maxE);
+  fangleGam      = new TH1F("Angle_gamma",     "Angle  Gamma",     100, -1.0,  1.0);
+  fenergyElec    = new TH1F("Energy_elec",     "Energy Electron",  100, 0., maxE);
+  fangleElec     = new TH1F("Angle_elec",      "Angle  Electron",  100, -1.0,  1.0);  
 }
 
+void GUHistogram::RecordHistos( double EnPrimary,
+                                double EnFinalGamma,
+                                double angleGamma,    
+                                double EnElectron,
+                                double angleElectron)
+{
+   fenergyPrimary->Fill(EnPrimary);
+   fenergyGam->Fill(EnFinalGamma);
+   fangleGam->Fill(angleGamma);
+   fenergyElec->Fill(EnElectron);
+   fangleElec->Fill(angleElectron);
+}
+
+void GUHistogram::RecordTime( double elapsedTime )
+{
+    ftime->Fill(elapsedTime);
+}
+   
 } // end namespace vecphys
