02815eba752eab5d747cc532e9aadeaa5fb530e9
Andrei Gheata <andrei.gheata@cern.ch>
2015-04-05 10:13:51 +0200

    Changed to log scale in X and decresed binning in CMS app

--- vecprot_v2/runCMS.C
+++ vecprot_v2/runCMS.C
@@ -59,8 +59,8 @@ void runCMS(Int_t nthreads=4,
 
    CMSApplication *app = new CMSApplication();
 //   app->SetScoreType(CMSApplication::kNoScore);
-//   app->SetScoreType(CMSApplication::kScoreStep);
-   app->SetScoreType(CMSApplication::kScoreEdep);
+   app->SetScoreType(CMSApplication::kScoreStep);
+//   app->SetScoreType(CMSApplication::kScoreEdep);
    prop->fApplication = app;
 
 //   gROOT->ProcessLine(".x factory.C+");   
