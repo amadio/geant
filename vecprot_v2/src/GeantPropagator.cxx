f84cf59fe695cca72986f3dd4cb24b12e3391652
Philippe Canal <pcanal@fnal.gov>
2015-04-29 15:34:58 -0500

    Initialize Application after loading the geometry (in case it needs it)

--- vecprot_v2/src/GeantPropagator.cxx
+++ vecprot_v2/src/GeantPropagator.cxx
@@ -298,8 +298,6 @@ void GeantPropagator::Initialize() {
     for (Int_t i = 0; i < fNthreads + 1; i++)
       fThreadData[i] = new GeantThreadData();
   }
-  // Initialize application
-  fApplication->Initialize();
 }
 
 #if USE_VECGEOM_NAVIGATOR == 1
@@ -383,6 +381,8 @@ void GeantPropagator::PropagatorGeom(const char *geomfile, Int_t nthreads, Bool_
   // Initialize geometry and current volume
   if (!LoadGeometry(geomfile))
     return;
+  // Initialize application
+  fApplication->Initialize();
   if (called) {
     Printf("Sorry, you can call this only once per session.");
     return;
