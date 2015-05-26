14925e6c735b84f9e7e256386965611e602bad7a
Federico Carminati <federico.carminati@cern.ch>
2015-05-26 15:20:35 +0200

    [GEANT-152] Moving to C++ compiler we need to stop demangling

--- tabxsec/src/fp_exception_glibc_extension.cc
+++ tabxsec/src/fp_exception_glibc_extension.cc
@@ -1,3 +1,4 @@
+extern "C" {
 /*   Title:  Floating-point exception handling example
     Author:  David N. Williams
       File:  fe-handlng-example.c
@@ -33,7 +34,7 @@ Numerics", 2004:
 
 http://lists.apple.com/archives/unix-porting/2003/May/msg00026.html
 
-Another version of the ppc legacy code is here: 
+Another version of the ppc legacy code is here:
 
 http://developer.apple.com/documentation/Performance/Conceptual/Mac_OSX_Numerics/Mac_OSX_Numerics.pdf
 
@@ -49,7 +50,7 @@ rather aborts with a diagnostic message.
 
 To compile it under Mac OS X, execute:
 
-  cc -o fe-handling fe-handling-example.c  
+  cc -o fe-handling fe-handling-example.c
 
 To compile it under Linux, execute:
 
@@ -80,14 +81,14 @@ http://graphviz.sourcearchive.com/documentation/2.16/gvrender__pango_8c-source.h
 
 #include <fenv.h>
 
-#define DEFINED_PPC      (defined(__ppc__) || defined(__ppc64__))
-#define DEFINED_INTEL    (defined(__i386__) || defined(__x86_64__))
+#define DEFINED_PPC (defined(__ppc__) || defined(__ppc64__))
+#define DEFINED_INTEL (defined(__i386__) || defined(__x86_64__))
 
 #ifndef linux
 #if DEFINED_PPC
 
-#define FE_EXCEPT_SHIFT 22  // shift flags right to get masks
-#define FM_ALL_EXCEPT    FE_ALL_EXCEPT >> FE_EXCEPT_SHIFT 
+#define FE_EXCEPT_SHIFT 22 // shift flags right to get masks
+#define FM_ALL_EXCEPT FE_ALL_EXCEPT >> FE_EXCEPT_SHIFT
 
 /* GNU C Library:
 http://www.gnu.org/software/libc/manual/html_node/Control-Functions.html
@@ -96,7 +97,7 @@ http://www.gnu.org/software/libc/manual/html_node/Control-Functions.html
 
        The function returns a bitmask of all currently enabled
        exceptions.  It returns -1 in case of failure.
-   
+
    The excepts argument appears in other functions in fenv.h,
    and corresponds to the FE_xxx exception flag constants.  It
    is unclear whether the bitmask is for the flags or the masks.
@@ -107,130 +108,112 @@ http://www.gnu.org/software/libc/manual/html_node/Control-Functions.html
    to suggest that it's the mask corresponding to bits in
    excepts that is returned.
 */
-int
-fegetexcept (void)
-{
+int fegetexcept(void) {
   static fenv_t fenv;
 
-  return ( fegetenv (&fenv) ? -1 :
-    (
-      ( fenv & (FM_ALL_EXCEPT) ) << FE_EXCEPT_SHIFT )
-    );
+  return (fegetenv(&fenv) ? -1 : ((fenv & (FM_ALL_EXCEPT)) << FE_EXCEPT_SHIFT));
 }
 
-int
-feenableexcept (unsigned int excepts)
-{
+int feenableexcept(unsigned int excepts) {
   static fenv_t fenv;
-  unsigned int new_excepts = (excepts & FE_ALL_EXCEPT) >> FE_EXCEPT_SHIFT,
-               old_excepts;  // all previous masks
+  unsigned int new_excepts = (excepts & FE_ALL_EXCEPT) >> FE_EXCEPT_SHIFT, old_excepts; // all previous masks
 
-  if ( fegetenv (&fenv) ) return -1;
+  if (fegetenv(&fenv))
+    return -1;
   old_excepts = (fenv & FM_ALL_EXCEPT) << FE_EXCEPT_SHIFT;
 
   fenv = (fenv & ~new_excepts) | new_excepts;
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+  return (fesetenv(&fenv) ? -1 : old_excepts);
 }
 
-int
-fedisableexcept (unsigned int excepts)
-{
+int fedisableexcept(unsigned int excepts) {
   static fenv_t fenv;
-  unsigned int still_on = ~( (excepts & FE_ALL_EXCEPT) >> FE_EXCEPT_SHIFT ),
-               old_excepts;  // previous masks
+  unsigned int still_on = ~((excepts & FE_ALL_EXCEPT) >> FE_EXCEPT_SHIFT), old_excepts; // previous masks
 
-  if ( fegetenv (&fenv) ) return -1;
+  if (fegetenv(&fenv))
+    return -1;
   old_excepts = (fenv & FM_ALL_EXCEPT) << FE_EXCEPT_SHIFT;
 
   fenv &= still_on;
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+  return (fesetenv(&fenv) ? -1 : old_excepts);
 }
 
 #elif DEFINED_INTEL
 
-int
-fegetexcept (void)
-{
+int fegetexcept(void) {
   static fenv_t fenv;
 
-  return fegetenv (&fenv) ? -1 : (fenv.__control & FE_ALL_EXCEPT);
+  return fegetenv(&fenv) ? -1 : (fenv.__control & FE_ALL_EXCEPT);
 }
 
-int
-feenableexcept (unsigned int excepts)
-{
+int feenableexcept(unsigned int excepts) {
   static fenv_t fenv;
-  unsigned int new_excepts = excepts & FE_ALL_EXCEPT,
-               old_excepts;  // previous masks
+  unsigned int new_excepts = excepts & FE_ALL_EXCEPT, old_excepts; // previous masks
 
-  if ( fegetenv (&fenv) ) return -1;
+  if (fegetenv(&fenv))
+    return -1;
   old_excepts = fenv.__control & FE_ALL_EXCEPT;
 
   // unmask
   fenv.__control &= ~new_excepts;
-  fenv.__mxcsr   &= ~(new_excepts << 7);
+  fenv.__mxcsr &= ~(new_excepts << 7);
 
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+  return (fesetenv(&fenv) ? -1 : old_excepts);
 }
 
-int
-fedisableexcept (unsigned int excepts)
-{
+int fedisableexcept(unsigned int excepts) {
   static fenv_t fenv;
-  unsigned int new_excepts = excepts & FE_ALL_EXCEPT,
-               old_excepts;  // all previous masks
+  unsigned int new_excepts = excepts & FE_ALL_EXCEPT, old_excepts; // all previous masks
 
-  if ( fegetenv (&fenv) ) return -1;
+  if (fegetenv(&fenv))
+    return -1;
   old_excepts = fenv.__control & FE_ALL_EXCEPT;
 
   // mask
   fenv.__control |= new_excepts;
-  fenv.__mxcsr   |= new_excepts << 7;
+  fenv.__mxcsr |= new_excepts << 7;
 
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+  return (fesetenv(&fenv) ? -1 : old_excepts);
 }
 
-#endif  // PPC or INTEL enabling
-#endif  // not linux
+#endif // PPC or INTEL enabling
+#endif // not linux
 
 #if DEFINED_PPC
 
-#define getfpscr(x)    asm volatile ("mffs %0" : "=f" (x));
-#define setfpscr(x)    asm volatile ("mtfsf 255,%0" : : "f" (x));
+#define getfpscr(x) asm volatile("mffs %0" : "=f"(x));
+#define setfpscr(x) asm volatile("mtfsf 255,%0" : : "f"(x));
 
 typedef union {
-    struct {
-        unsigned long hi;
-        unsigned long lo;
-    } i;
-    double d;
+  struct {
+    unsigned long hi;
+    unsigned long lo;
+  } i;
+  double d;
 } hexdouble;
 
-#endif  // DEFINED_PPC
+#endif // DEFINED_PPC
 
 #if DEFINED_INTEL
 
 // x87 fpu
-#define getx87cr(x)    asm ("fnstcw %0" : "=m" (x));
-#define setx87cr(x)    asm ("fldcw %0"  : "=m" (x));
-#define getx87sr(x)    asm ("fnstsw %0" : "=m" (x));
+#define getx87cr(x) asm("fnstcw %0" : "=m"(x));
+#define setx87cr(x) asm("fldcw %0" : "=m"(x));
+#define getx87sr(x) asm("fnstsw %0" : "=m"(x));
 
 // SIMD, gcc with Intel Core 2 Duo uses SSE2(4)
-#define getmxcsr(x)    asm ("stmxcsr %0" : "=m" (x));
-#define setmxcsr(x)    asm ("ldmxcsr %0" : "=m" (x));
+#define getmxcsr(x) asm("stmxcsr %0" : "=m"(x));
+#define setmxcsr(x) asm("ldmxcsr %0" : "=m"(x));
 
-#endif  // DEFINED_INTEL
+#endif // DEFINED_INTEL
 
 #include <signal.h>
-#include <stdio.h>   // printf()
-#include <stdlib.h>  // abort(), exit()
+#include <stdio.h>  // printf()
+#include <stdlib.h> // abort(), exit()
 
-static const char *fe_code_name[] = {
-  "FPE_NOOP",
-  "FPE_FLTDIV", "FPE_FLTINV", "FPE_FLTOVF", "FPE_FLTUND",
-  "FPE_FLTRES", "FPE_FLTSUB", "FPE_INTDIV", "FPE_INTOVF"
-  "FPE_UNKNOWN"
-};
+static const char *fe_code_name[] = {"FPE_NOOP", "FPE_FLTDIV", "FPE_FLTINV", "FPE_FLTOVF", "FPE_FLTUND", "FPE_FLTRES",
+                                     "FPE_FLTSUB", "FPE_INTDIV", "FPE_INTOVF"
+                                                                 "FPE_UNKNOWN"};
 
 /* SAMPLE ALTERNATE FP EXCEPTION HANDLER
 
@@ -246,58 +229,72 @@ static const char *fe_code_name[] = {
        fpu (but not the SIMD unit, nor the ppc)
      * imprecision of interrupts from system software
 */
-void
-fhdl ( int sig, siginfo_t *sip, ucontext_t *scp )
-{
+void fhdl(int sig, siginfo_t *sip, ucontext_t * /*scp*/) {
   int fe_code = sip->si_code;
-  unsigned int excepts = fetestexcept (FE_ALL_EXCEPT);
+  unsigned int excepts = fetestexcept(FE_ALL_EXCEPT);
 
-  switch (fe_code)
-  {
-#ifdef FPE_NOOP  // occurs in OS X
-    case FPE_NOOP:   fe_code = 0; break;
+  switch (fe_code) {
+#ifdef FPE_NOOP // occurs in OS X
+  case FPE_NOOP:
+    fe_code = 0;
+    break;
 #endif
-    case FPE_FLTDIV: fe_code = 1; break; // divideByZero
-    case FPE_FLTINV: fe_code = 2; break; // invalid
-    case FPE_FLTOVF: fe_code = 3; break; // overflow
-    case FPE_FLTUND: fe_code = 4; break; // underflow
-    case FPE_FLTRES: fe_code = 5; break; // inexact
-    case FPE_FLTSUB: fe_code = 6; break; // invalid
-    case FPE_INTDIV: fe_code = 7; break; // overflow
-    case FPE_INTOVF: fe_code = 8; break; // underflow
-            default: fe_code = 9;
-   }
+  case FPE_FLTDIV:
+    fe_code = 1;
+    break; // divideByZero
+  case FPE_FLTINV:
+    fe_code = 2;
+    break; // invalid
+  case FPE_FLTOVF:
+    fe_code = 3;
+    break; // overflow
+  case FPE_FLTUND:
+    fe_code = 4;
+    break; // underflow
+  case FPE_FLTRES:
+    fe_code = 5;
+    break; // inexact
+  case FPE_FLTSUB:
+    fe_code = 6;
+    break; // invalid
+  case FPE_INTDIV:
+    fe_code = 7;
+    break; // overflow
+  case FPE_INTOVF:
+    fe_code = 8;
+    break; // underflow
+  default:
+    fe_code = 9;
+  }
 
-  if ( sig == SIGFPE )
-  {
+  if (sig == SIGFPE) {
 #if DEFINED_INTEL
-    unsigned short x87cr,x87sr;
+    unsigned short x87cr, x87sr;
     unsigned int mxcsr;
 
-    getx87cr (x87cr);
-    getx87sr (x87sr);
-    getmxcsr (mxcsr);
-    printf ("X87CR:   0x%04X\n", x87cr);
-    printf ("X87SR:   0x%04X\n", x87sr);
-    printf ("MXCSR:   0x%08X\n", mxcsr);
+    getx87cr(x87cr);
+    getx87sr(x87sr);
+    getmxcsr(mxcsr);
+    printf("X87CR:   0x%04X\n", x87cr);
+    printf("X87SR:   0x%04X\n", x87sr);
+    printf("MXCSR:   0x%08X\n", mxcsr);
 #endif
 
 #if DEFINED_PPC
-   hexdouble t;
+    hexdouble t;
 
-   getfpscr (t.d);
-   printf ("FPSCR:   0x%08X\n", t.i.lo);
+    getfpscr(t.d);
+    printf("FPSCR:   0x%08X\n", t.i.lo);
 #endif
 
-    printf ("signal:  SIGFPE with code %s\n", fe_code_name[fe_code]);
-    printf ("invalid flag:    0x%04X\n", excepts & FE_INVALID);
-    printf ("divByZero flag:  0x%04X\n", excepts & FE_DIVBYZERO);
-  }
-  else printf ("Signal is not SIGFPE, it's %i.\n", sig);
+    printf("signal:  SIGFPE with code %s\n", fe_code_name[fe_code]);
+    printf("invalid flag:    0x%04X\n", excepts & FE_INVALID);
+    printf("divByZero flag:  0x%04X\n", excepts & FE_DIVBYZERO);
+  } else
+    printf("Signal is not SIGFPE, it's %i.\n", sig);
 
   abort();
 }
-
 
 /*
 int main (int argc, char **argv)
@@ -308,7 +305,7 @@ int main (int argc, char **argv)
     act.sa_sigaction = (void(*))fhdl;
     sigemptyset (&act.sa_mask);
     act.sa_flags = SA_SIGINFO;
-    
+
 
 //  printf ("Old divByZero exception: 0x%08X\n", feenableexcept (FE_DIVBYZERO));
     printf ("Old invalid exception:   0x%08X\n", feenableexcept (FE_INVALID));
@@ -326,3 +323,4 @@ int main (int argc, char **argv)
     return 0;
 }
 */
+}
