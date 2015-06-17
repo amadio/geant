ba7a626968a636703b9587a4cfbf4723ba0dbec3
Soon Yung Jun <syjun@mic.fnal.gov>
2015-06-17 15:34:42 -0500

    VECPHYS_GLOBAL in PhysicalConstants and SystemOfUnit was not respected in table buildings - switch back to const temporarily

--- GXTracking/GUTracking/inc/SystemOfUnits.h
+++ GXTracking/GUTracking/inc/SystemOfUnits.h
@@ -8,31 +8,31 @@
 namespace vecphys {
 
 // length
-VECPHYS_GLOBAL double mm = 1.0;
-VECPHYS_GLOBAL double cm = 10.*mm;
-VECPHYS_GLOBAL double  m = 1.e+3*mm;
-VECPHYS_GLOBAL double  meter = 1.e+3*mm;
+const double mm = 1.0;
+const double cm = 10.*mm;
+const double  m = 1.e+3*mm;
+const double  meter = 1.e+3*mm;
 
 // time
-VECPHYS_GLOBAL double ns = 1.0;
-VECPHYS_GLOBAL double  s = 1.e+9*ns;
-VECPHYS_GLOBAL double ms = 1.e-3*s;
-VECPHYS_GLOBAL double second = s;
+const double ns = 1.0;
+const double  s = 1.e+9*ns;
+const double ms = 1.e-3*s;
+const double second = s;
 
 // energy
-VECPHYS_GLOBAL double MeV = 1.0;
-VECPHYS_GLOBAL double TeV = 1.e+6*MeV;
-VECPHYS_GLOBAL double GeV = 1.e+3*MeV;
-VECPHYS_GLOBAL double keV = 1.e-3*MeV;
-VECPHYS_GLOBAL double  eV = 1.e-6*MeV;
-VECPHYS_GLOBAL double joule = 6.24150e+12*MeV;
+const double MeV = 1.0;
+const double TeV = 1.e+6*MeV;
+const double GeV = 1.e+3*MeV;
+const double keV = 1.e-3*MeV;
+const double  eV = 1.e-6*MeV;
+const double joule = 6.24150e+12*MeV;
 
 // mass 
-VECPHYS_GLOBAL double gram = 1.e-3*joule*s*s/(m*m);
+const double gram = 1.e-3*joule*s*s/(m*m);
 
 // cross section
-VECPHYS_GLOBAL double barn = 1.e-28*m*m;
-VECPHYS_GLOBAL double microbarn = 1.e-6*barn;
+const double barn = 1.e-28*m*m;
+const double microbarn = 1.e-6*barn;
 
 } // end namespace vecphys
 
