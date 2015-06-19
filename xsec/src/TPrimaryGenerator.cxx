49533e83942de6651c46099e22497d989cf74c03
Philippe Canal <pcanal@fnal.gov>
2015-06-19 10:28:46 -0500

    White space

--- xsec/src/TPrimaryGenerator.cxx
+++ xsec/src/TPrimaryGenerator.cxx
@@ -7,8 +7,8 @@
 
 ClassImp(TPrimaryGenerator)
 
-    //______________________________________________________________________________
-    TPrimaryGenerator::TPrimaryGenerator()
+//______________________________________________________________________________
+TPrimaryGenerator::TPrimaryGenerator()
     : fPDG(11),                        // PDG code of the primary: 11 -> e-
       fPartEkin(0.03),                 // kinetic energy of the primary [GeV] : 30 MeV
       fXPos(0.),                       // (x,y,z) position of the primary particles: (0,0,0)
