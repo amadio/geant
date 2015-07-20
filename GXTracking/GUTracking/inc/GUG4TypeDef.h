12418d4b1eb7435be531a6e89c5639fea7c8f7bc
Soon Yung Jun <syjun@mic.fnal.gov>
2015-07-20 18:25:27 -0500

    temporary typedef used for validation to Geant4

---
+++ GXTracking/GUTracking/inc/GUG4TypeDef.h
@@ -0,0 +1,17 @@
+#ifndef GUG4TypeDef_HH
+#define GUG4TypeDef_HH 1
+
+// this is temporary only for the purpose of validation w.r.t Geant4 - syjun
+
+// Geant4 type definition
+
+typedef bool G4bool;
+typedef int G4int;
+typedef double G4double;
+
+// Geant4 function definition
+#define G4Log(X) log(X)
+#define G4Exp(X) exp(X)
+
+
+#endif
