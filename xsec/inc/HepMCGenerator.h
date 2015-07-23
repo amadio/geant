270f7096e63fc11757d2bedd0a6832d16d886079
Federico Carminati <federico.carminati@cern.ch>
2015-07-23 11:20:38 +0200

    GEANT-133 Using VecGeom Particle type

--- xsec/inc/HepMCGenerator.h
+++ xsec/inc/HepMCGenerator.h
@@ -18,33 +18,31 @@ using vecgeom::Particle;
 class TParticlePDG;
 #endif
 
-class HepMCGenerator: public PrimaryGenerator {
- private:
-
+class HepMCGenerator : public PrimaryGenerator {
+private:
 #if __cplusplus >= 201103L
-    HepMC::Reader* input_file;
-    HepMC::FindParticles* search;
+  HepMC::Reader *input_file;
+  HepMC::FindParticles *search;
 #endif
-    
- public:
-    HepMCGenerator();
-    HepMCGenerator(std::string& filename);
-    ~HepMCGenerator();
+
+public:
+  HepMCGenerator();
+  HepMCGenerator(std::string &filename);
+  ~HepMCGenerator();
 
   // set one GeantTrack primary track properties
-    virtual void InitPrimaryGenerator();
-    virtual Int_t NextEvent();
-    virtual void GetTrack(Int_t n, Geant::GeantTrack &gtrack);
- // used from Geant4 test-complex to take one primary track
-    void GetTrack(Int_t n, Double_t &tpx, Double_t &tpy, Double_t &tpz, Double_t &te,
-                  Double_t &x0, Double_t &y0, Double_t &z0, Int_t &pdg);
+  virtual void InitPrimaryGenerator();
+  virtual Int_t NextEvent();
+  virtual void GetTrack(Int_t n, Geant::GeantTrack &gtrack);
+  // used from Geant4 test-complex to take one primary track
+  void GetTrack(Int_t n, Double_t &tpx, Double_t &tpy, Double_t &tpz, Double_t &te, Double_t &x0, Double_t &y0,
+                Double_t &z0, Int_t &pdg);
 
- private:
+private:
+  HepMCGenerator(const HepMCGenerator &); // no imp.
+  HepMCGenerator &operator=(const HepMCGenerator &); // no imp.
 
-   HepMCGenerator(const HepMCGenerator &);//no imp.	
-   HepMCGenerator& operator=(const HepMCGenerator &);//no imp.
-
-   ClassDef(HepMCGenerator,1)
+  ClassDef(HepMCGenerator, 1)
 };
 
 #endif
