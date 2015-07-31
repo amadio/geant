24b5bbbcbb2c82c26c93b7d32febf83d0d0b5dcc
Anmol Arora <anmolarora@pb-d-128-141-192-103.cern.ch>
2015-07-31 12:59:46 +0200

    Removal of FORTRAN

--- Nudy/inc/TNudyDB.h
+++ Nudy/inc/TNudyDB.h
@@ -1,5 +1,5 @@
-#ifndef ROOT_TNudyDB
-#define ROOT_TNudyDB
+#ifndef TNudyDB_H
+#define TNudyDB_H
 
 #include <THashTable.h>
 #include <TFile.h>
@@ -8,18 +8,20 @@
 #include "TNudyCore.h"
 #include "TNudyLibrary.h"
 
-class TNudyDB : public TNamed {
-public:
-  //   TNudyDB(const char *name, const char *title, const char *file);
-  TNudyDB(const char *name, const char *title, const char *file); // DEBUG
-  virtual ~TNudyDB();
-  void AddLibrary(const char *name, const char *file); // Add a Library to a Nudy Database from a RENDF file
-  void RemoveLibrary(const char *name);                // Remove a Library from a Nudy Databse
-  TList *GetEntries();                                 // Get a List of Libraries in the Nudy Database
-  TFile *GetDBFile() { return fDB; }                   // Get a pointer to the database file
-private:
-  TFile *fDB; //! Database data file
 
-  ClassDef(TNudyDB, 1)
+class TNudyDB : public TNamed {
+ public:
+  //   TNudyDB(const char *name, const char *title, const char *file);
+  TNudyDB(const char *name, const char *title, const char *file);// DEBUG
+  virtual ~TNudyDB();
+  void AddLibrary(const char *name, const char* file); //Add a Library to a Nudy Database from a RENDF file
+  void RemoveLibrary(const char *name); //Remove a Library from a Nudy Databse
+  TList* GetEntries();  //Get a List of Libraries in the Nudy Database
+  TFile* GetDBFile() {return fDB;} //Get a pointer to the database file
+ private:
+  
+  TFile *fDB; //! Database data file
+  
+  ClassDef(TNudyDB,1)
 };
 #endif
