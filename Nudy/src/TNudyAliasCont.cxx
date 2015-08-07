f13994190027cc608b59d11ab622a214d9e71c63
Federico Carminati <federico.carminati@cern.ch>
2015-08-07 17:12:17 +0200

    GEANT-133 Fixing CIM 62704 Uninitialized pointer field

--- Nudy/src/TNudyAliasCont.cxx
+++ Nudy/src/TNudyAliasCont.cxx
@@ -2,22 +2,24 @@
 
 ClassImp(TNudyAliasCont)
 
-//_______________________________________________________________________________
-TNudyAliasCont::TNudyAliasCont(): fLen(0), fChooseBin(NULL), fP(NULL), fX(NULL), fInterX(NULL),
-   fInterP(NULL), fTx(NULL), fTp(NULL), fTa(-1), fInterAlpha(10), fRan(NULL), fRnd(NULL),
-   fAlpha(10)
+    //_______________________________________________________________________________
+    TNudyAliasCont::TNudyAliasCont()
+    : fLen(0), fChooseBin(NULL), fP(NULL), fX(NULL), fInterX(NULL), fInterP(NULL), fTx(NULL), fTp(NULL), fTa(-1),
+      fInterAlpha(10), fRan(NULL), fRnd(NULL), fAlpha(10)
 #ifdef TNUDYALIAS_MULTITHREAD
-   , fMult(NULL), fMultLen(0)
+      ,
+      fMult(NULL), fMultLen(0)
 #endif
-{}
+{
+}
 
 //_______________________________________________________________________________
-TNudyAliasCont::TNudyAliasCont(TArrayD *data, double alpha, unsigned int seed): 
-   fLen(0), fChooseBin(NULL), fP(NULL), fX(NULL), fInterX(NULL),
-   fInterP(NULL), fTx(NULL), fTp(NULL), fTa(-1), fInterAlpha(10), fRan(NULL), fRnd(NULL),
-   fAlpha(10)
+TNudyAliasCont::TNudyAliasCont(TArrayD *data, double alpha, unsigned int seed)
+    : fLen(0), fChooseBin(NULL), fP(NULL), fX(NULL), fInterX(NULL), fInterP(NULL), fTx(NULL), fTp(NULL), fTa(-1),
+      fInterAlpha(10), fRan(NULL), fRnd(NULL), fAlpha(10)
 #ifdef TNUDYALIAS_MULTITHREAD
-   , fMult(NULL), fMultLen(0)
+      ,
+      fMult(NULL), fMultLen(0)
 #endif
 {
   // TArrayD of x1,p1,x2,p2.....xn,pn
