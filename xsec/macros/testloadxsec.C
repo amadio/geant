9266f666c367d49ba355dbb375cae732667f6e38
Federico Carminati <federico.carminati@cern.ch>
2015-10-06 00:20:21 +0200

    GEANT-160 Fixing for ROOT6 and new formats

--- xsec/macros/testloadxsec.C
+++ xsec/macros/testloadxsec.C
@@ -1,3 +1,9 @@
+#include "TSystem.h"
+#include "TGeoManager.h"
+
+#include "TTabPhysMgr.h"
+#include "GeantPropagator.h"
+
 void testloadxsec()
 {
    gSystem->Load("libPhysics.so");
@@ -5,13 +11,16 @@ void testloadxsec()
    gSystem->Load("libThread.so");
    gSystem->Load("libGeom.so");
    gSystem->Load("libVMC.so");
-	gSystem->Load("../../lib/libXsec");
+   gSystem->Load("../../lib/libXsec");
    gSystem->Load("../../lib/libGeant_v.so");
    gSystem->Load("../../lib/libUser.so");
-	TGeoManager *geom;
-//	geom = TGeoManager::Import("Al_H2O_H.root");
-	geom = TGeoManager::Import("http://root.cern.ch/files/cms.root");
-	TTabPhysMgr::Instance(geom, "xsec_FTFP_BERT.root", "fstate_FTFP_BERT.root" );
+   TGeoManager *geom = nullptr;
+   //	geom = TGeoManager::Import("Al_H2O_H.root");
+   const char *fxsec = "../../data/xsec_FTFP_BERT_G496p02_1mev.root";
+   const char *ffins = "../../data/fstate_FTFP_BERT_G496p02_1mev.root";
+   GeantPropagator::Instance(1,1,1);
+   geom = TGeoManager::Import("http://root.cern.ch/files/cms.root");
+   TTabPhysMgr::Instance(fxsec, ffins );
    delete geom;
    delete TTabPhysMgr::Instance();
 }
