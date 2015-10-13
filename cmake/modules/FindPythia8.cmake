62570cc38b014a0ab3a7599e99e0d217b0633271
Oksana Shadura <oksana.shadura@cern.ch>
2015-10-13 13:47:57 +0200

    Temporary fix in FindPythia8.cmake to made it compatible also with AFS standalone builds of Pythia8

--- cmake/modules/FindPythia8.cmake
+++ cmake/modules/FindPythia8.cmake
@@ -1,15 +1,32 @@
-# Try to find Pythia8
+# Defines:
+#
+#  PYTHIA8_FOUND
+#  PYTHIA8_VERSION
+#  PYTHIA8_INCLUDE_DIR
+#  PYTHIA8_INCLUDE_DIRS (not cached)
+#  PYTHIA8_LIBRARY
+#  PYTHIA8_hepmcinterface_LIBRARY
+#  PYTHIA8_lhapdfdummy_LIBRARY
+#  PYTHIA8_LIBRARIES (not cached) : for PYTHIA8_VERSION < 200 includes 3 libraries above; not to be used if lhapdf is used
 
-find_path(PYTHIA8_INCLUDE_DIR Pythia8/Pythia.h PATH_SUFFIXES Pythia8)
 
-find_library(LHAPDFDUMMY_LIBRARY NAMES liblhapdfdummy lhapdfdummy)
-find_library(PYTHIA8_LIBRARY NAMES pythia8 libpythia8)
+find_path(PYTHIA8_INCLUDE_DIR Pythia.h Pythia8/Pythia.h
+          HINTS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include)
 
-set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR})
-set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${LHAPDFDUMMY_LIBRARY})
+find_library(PYTHIA8_LIBRARY NAMES pythia8 Pythia8
+             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Pythia8 DEFAULT_MSG
-	LHAPDFDUMMY_LIBRARY PYTHIA8_LIBRARY PYTHIA8_INCLUDE_DIR)
+find_library(PYTHIA8_lhapdfdummy_LIBRARY NAMES lhapdfdummy
+               HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
 
-mark_as_advanced(PYTHIA8_INCLUDE_DIR LHAPDFDUMMY_LIBRARY PYTHIA8_LIBRARY)
+set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${PYTHIA8_lhapdfdummy_LIBRARY})
+
+
+set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR} ${PYTHIA8_INCLUDE_DIR}/Pythia8 )
+
+# handle the QUIETLY and REQUIRED arguments and set PYTHIA8_FOUND to TRUE if
+# all listed variables are TRUE
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
