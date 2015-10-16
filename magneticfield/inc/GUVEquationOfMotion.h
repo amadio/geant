83d21e07db7b641dd3844c056ff3ce57a7dd7a49
John Apostolakis <john.apostolakis@cern.ch>
2015-10-16 17:07:44 +0200

    - Took 'charge' out of method interface
    - Took charge out of Evaluate method interface
    - Added methods to initialize charge, finish
    - Fixed size of PositionAndTime[4]

--- magneticfield/inc/GUVEquationOfMotion.h
+++ magneticfield/inc/GUVEquationOfMotion.h
@@ -13,6 +13,8 @@
 #ifndef GUV_EquationOfMotion_DEF
 #define GUV_EquationOfMotion_DEF
 
+#include <cassert>
+
 // #include "GUVTypes.hh"      // "globals.hh"
 #include "GUVField.h"   // required in inline method implementations
 
@@ -22,36 +24,44 @@ class GUVEquationOfMotion
 
      GUVEquationOfMotion( GUVField *Field );
      virtual ~GUVEquationOfMotion();
-       // Constructor and virtual destructor. No operations.
+       // Constructor and virtual destructor. No operations, just checks
 
      virtual void EvaluateRhsGivenB( const  double y[],
                                      const  double B[3],
-                                     double charge,
-                                     double dydx[] ) const = 0;
+                                     /*  double charge, */
+                                            double dydx[] ) const = 0;
        // Given the value of the  field "B", this function 
        // calculates the value of the derivative dydx.
        // --------------------------------------------------------
        // This is the _only_ function a subclass must define.
        // The other two functions use Rhs_givenB.
 
+      virtual void InitializeCharge(double particleCharge)=0;
+       // Must be called to correctly initialise and provide charge
+      virtual void InvalidateParameters()=0;
+      
+      inline void InformReady(); // All parameters have been set (charge+)
+      inline void InformDone();  // Invalidate charge, other parameters
+      inline void CheckInitialization() const; // Ensure initialization
+      inline void CheckDone() const;
+      
      // virtual void SetChargeMomentumMass(double particleCharge,
      //                                    double MomentumXc,
      //                                    double MassXc2) = 0;
      //   // Set the charge, momentum and mass of the current particle
      //   // --> used to set the equation's coefficients ...
 
-     inline
-     void RightHandSide( const  double y[],
-                                double charge ,
-                                double dydx[] ) const;
+      inline void RightHandSide( const  double y[],
+                                     // double charge ,
+                                        double dydx[] ) const;
        // This calculates the value of the derivative dydx at y.
        // It is the usual enquiry function.
        // ---------------------------
-       // (It uses the virtual function above.)
+       // It uses the virtual function EvaluateRhsGivenB
 
      void EvaluateRhsReturnB( const  double y[],
                               double dydx[],
-                              double charge,
+                           // double charge,
                               double Field[] ) const;
        // Same as RHS above, but also returns the value of B.
        // Should be made the new default ? after putting dydx & B in a class.
@@ -73,7 +83,7 @@ class GUVEquationOfMotion
      enum { GUVmaximum_number_of_field_components = 24 } ;
 
      GUVField *fField;
-
+     bool      fInitialised;
 };
 
 // #include "GUVEquationOfMotion.icc"
@@ -84,8 +94,23 @@ class GUVEquationOfMotion
 
 inline
 GUVEquationOfMotion::GUVEquationOfMotion(GUVField* pField) 
-  :fField(pField)
+   :fField(pField),  fInitialised(false)
 {}
+
+inline
+void GUVEquationOfMotion::InformReady() // was Initialize()
+{
+   assert( ! fInitialised ); // Sanity checking - assumes Clear() is always called!
+   fInitialised= true;
+}
+
+
+inline
+void GUVEquationOfMotion::InformDone()  // was Clear() and before Finished(); 
+{
+   assert( fInitialised ); 
+   fInitialised= false;   
+}  
 
 inline
 void GUVEquationOfMotion::GetFieldValue( const  double Point[4],
@@ -97,21 +122,42 @@ void GUVEquationOfMotion::GetFieldValue( const  double Point[4],
 inline
 void 
 GUVEquationOfMotion::RightHandSide( const  double y[],
-           double charge, double dydx[]  ) const
+                                       //  double charge,
+                                           double dydx[]  ) const
 {
-     double Field[GUVmaximum_number_of_field_components];   
-     double PositionAndTime[4];
-
+   CheckInitialization(); 
+     
+   double Field[GUVmaximum_number_of_field_components];   
+   double PositionAndTime[4];
+   
      // Position
-     PositionAndTime[0] = y[0];
-     PositionAndTime[1] = y[1];
-     PositionAndTime[2] = y[2];
-     // Global Time
-     PositionAndTime[3] = y[idxTime];  // See GUVFieldTrack::LoadFromArray
-
-     GetFieldValue(PositionAndTime, Field) ;
-     EvaluateRhsGivenB( y, Field, charge, dydx );
+   PositionAndTime[0] = y[0];
+   PositionAndTime[1] = y[1];
+   PositionAndTime[2] = y[2];
+   // Global Time
+   PositionAndTime[3] = y[idxTime];  // See GUVFieldTrack::LoadFromArray
+   
+   GetFieldValue(PositionAndTime, Field) ;
+   EvaluateRhsGivenB( y, Field, /*charge,*/ dydx );
 }
 
+#include <iostream>
+
+void GUVEquationOfMotion::CheckInitialization() const
+{
+   if( !fInitialised ){
+      std::cerr << "GUVEquationOfMotion is not Initialised" << std::endl; 
+   }
+   assert( fInitialised );
+}
+
+
+void GUVEquationOfMotion::CheckDone() const
+{
+   if( fInitialised ){
+      std::cerr << "GUVEquationOfMotion was NOT told it is Done!" << std::endl; 
+   }
+   assert( !fInitialised );
+}
 
 #endif /* GUV_EquationOfMotion_DEF */
