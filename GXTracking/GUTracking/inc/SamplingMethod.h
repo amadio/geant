ed3459582c4d7587faedff7ffcf2376ce33dd02a
Soon Yung Jun <syjun@mic.fnal.gov>
2015-10-28 18:59:08 -0500

    add a temporary utility header for sampling types

---
+++ GXTracking/GUTracking/inc/SamplingMethod.h
@@ -0,0 +1,16 @@
+#ifndef SamplingMethod_H
+#define SamplingMethod_H 
+
+namespace vecphys {
+
+enum SamplingMethod {
+  kNullMethod = -1, 
+  kAlias,                //Alias method 
+  kRejection,            //Geant4 CompositionAndRejection
+  kUnpack,               //Shuffling
+  kNumberSamplingMethod
+};
+
+} // end namespace vecphys
+
+#endif
