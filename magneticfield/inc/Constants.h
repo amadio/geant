dde118ea7236e646ceabd8ad8fb269ea286df9ae
John Apostolakis <john.apostolakis@cern.ch>
2015-11-03 10:46:44 +0100

    Moved c_light from Units.h to Constants.h

--- magneticfield/inc/Constants.h
+++ magneticfield/inc/Constants.h
@@ -1,10 +1,15 @@
 #ifndef FIELD_CONSTANTS_H
 #define FIELD_CONSTANTS_H 1
 
+#include "Units.h"
+
 namespace fieldConstants {
    
 static constexpr double     pi = 3.1415926535897932384626;
 static constexpr double  twopi = 2.0 * pi;
+
+static constexpr double c_light   = 2.99792458e+8 * fieldUnits::meter/fieldUnits::second;
+static constexpr double c_squared = c_light * c_light;
 
 };
 #endif
