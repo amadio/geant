2c0dc7bb991c0cb77e402d29f92adf74f808e006
Soon Yung Jun <syjun@mic.fnal.gov>
2015-11-10 23:05:12 -0600

    more efficient calcuation for evaluating the fractional position of energy within the target bin

--- GXTracking/GUTracking/inc/Power2Divisor.h
+++ GXTracking/GUTracking/inc/Power2Divisor.h
@@ -11,29 +11,10 @@ class Power2Divisor
 { 
 public:
   VECPHYS_CUDA_HEADER_BOTH
-  Power2Divisor(int nbins, int ndivs, Precision scale);
+  Power2Divisor(int nmin, int nmax, int ndiv);
 
-  template <typename Backend>
   VECPHYS_CUDA_HEADER_BOTH
-  typename Backend::Index_t
-  GetBin(typename Backend::Double_t x);
-
-  template <typename Backend>
-  inline 
-  VECPHYS_CUDA_HEADER_BOTH
-  typename Backend::Double_t
-  GetBinPosition(typename Backend::Index_t ibin);
-
-  template <typename Backend>
-  VECPHYS_CUDA_HEADER_BOTH
-  typename Backend::Double_t
-  GetBinSize(typename Backend::Index_t ibin);
-
-  template <typename Backend>
-  VECPHYS_CUDA_HEADER_BOTH
-  typename Backend::Double_t
-  FractionWithinBin(typename Backend::Double_t x,
-                    typename Backend::Index_t ibin);
+  int GetNumberOfBins();
 
   VECPHYS_CUDA_HEADER_BOTH
   Precision GetLowerBound() { return fLowerBound; }
@@ -41,92 +22,60 @@ public:
   VECPHYS_CUDA_HEADER_BOTH
   Precision GetUpperBound() { return fUpperBound; }
 
+  VECPHYS_CUDA_HEADER_BOTH
+  Precision GetBinPosition(int ibin);
+
+  VECPHYS_CUDA_HEADER_BOTH
+  Precision GetBinSize(int ibin);
+
+  template <typename Backend>
+  VECPHYS_CUDA_HEADER_BOTH
+  void GetBinAndFraction(typename Backend::Double_t x, 
+                         typename Backend::Index_t& ibin,
+                         typename Backend::Double_t& frac); 
+
 private:
-  int fNbins;              // number of bins
-  int fNdivs;              // number of equal grids between 2^e and 2^{e+1}
-  Precision fScale;        // scale for unit conversion (power of 10)
+  VECPHYS_CUDA_HEADER_BOTH
+  int Power2Exponent(int ibin);
 
-  Precision fLowerBound;   // lower bound of the range
-  Precision fUpperBound;   // upper bound of the range
+private:
+  int fNmin;              // minimum of the power2 exponent
+  int fNmax;              // maximum of the power2 exponent
+  int fNdiv;              // number of equal divisions between 2^e and 2^{e+1}
 
-  Precision* fBinPosition; // bin position array
-} ;
+  Precision fLowerBound;   // lower bound of the power2 range
+  Precision fUpperBound;   // upper bound of the power2 range
+};
 
 template <typename Backend>
 VECPHYS_CUDA_HEADER_BOTH
-typename Backend::Index_t
-Power2Divisor::GetBin(typename Backend::Double_t x) 
+void Power2Divisor::GetBinAndFraction(typename Backend::Double_t x, 
+                                      typename Backend::Index_t& ibin,
+                                      typename Backend::Double_t& frac) 
 {
   typedef typename Backend::Int_t Int_t;
+  typedef typename Backend::Index_t Index_t;
   typedef typename Backend::Double_t Double_t;
 
   Int_t    exponent;
-  Double_t mantissa = frexp (x/fScale, &exponent); // Vc::frexp
-  Double_t fexponent = IntToDouble(exponent);
-  return Floor((mantissa-.5)*(2.*fNdivs)) + 1. + fNdivs*(fexponent-1.);   
-}
+  Double_t mantissa = frexp (x, &exponent); // Vc::frexp
 
-template <typename Backend>
-inline
-VECPHYS_CUDA_HEADER_BOTH
-typename Backend::Double_t
-Power2Divisor::GetBinPosition(typename Backend::Index_t ibin) 
-{
-  //check for bounds
-  return fBinPosition[ibin-1];   
+  Double_t fexponent = IntToDouble(exponent-1-fNmin); //Backend
+  //note: the normal  conversion from int to double, 
+  //Double_t fexponent(exponent-1-fNmin)
+  //does not work for the int output of frexp which is in [int,dummy,int,dummy]
 
-  /* 
-  // vectorized version - still expensive
-  typedef typename Backend::Int_t Int_t;
-  typedef typename Backend::Double_t Double_t;
+  ibin = Floor((mantissa-.5)*(2.*fNdiv)) + fNdiv*fexponent;   
 
-  //no Vc % op, so do a%n = a - (n*int(a/n))
-  Int_t exponent2 = DoubleToInt((ibin-1.)/fNdivs);
-  Double_t mod = (ibin-1.)-fNdivs*IntToDouble(exponent2);
+  Index_t idiv = ibin - fNdiv*Floor(ibin/fNdiv); //idiv = ibin%fNdiv 
+  //note: ibin%fNdiv = ibin & (fNdiv-1) for any fNdiv = 2^n does not work here 
+  //as the & operator is not vectorized)
 
-  //bin position = pow(2,int((i-1)/fNdivs))*(1.+((i-1)%fNdivs)/fNdivs) 
-  Double_t binPosition = ldexp(1.+ mod/fNdivs, exponent2); // Vc::ldexp 
-  return fScale*binPosition;
-  */
-}
+  Double_t  power2  = ldexp(1.,exponent -1);
+  Double_t  binsize = power2/fNdiv;
+  Double_t  binloc  = power2 + binsize*idiv;
 
-// Specialisation for Vc Backend
-#ifndef VECPHYS_NVCC
-template<>
-inline
-VECPHYS_CUDA_HEADER_BOTH
-kVc::Double_t
-Power2Divisor::GetBinPosition<kVc>(typename kVc::Index_t ibin) 
-{
-  kVc::Double_t pos;
-  for(int i = 0; i < kVc::kSize ; ++i) {
-    int idx = ibin[i];
-    pos[i] = fBinPosition[idx];
-  }
-  return pos;   
-}
-#endif
-
-template <typename Backend>
-VECPHYS_CUDA_HEADER_BOTH
-typename Backend::Double_t
-Power2Divisor::GetBinSize(typename Backend::Index_t ibin) 
-{
-  return GetBinPosition<Backend>(ibin+1) - GetBinPosition<Backend>(ibin);
-}
-
-template <typename Backend>
-VECPHYS_CUDA_HEADER_BOTH
-typename Backend::Double_t
-Power2Divisor::FractionWithinBin(typename Backend::Double_t x, 
-                                 typename Backend::Index_t ibin) 
-{
-  typedef typename Backend::Double_t Double_t;
-
-  Double_t down = GetBinPosition<Backend>(ibin);
-  Double_t up   = GetBinPosition<Backend>(ibin+1);
-
-  return (x - down)/(up-down);
+  frac = (x-binloc)/binsize;
 }
 
 } // end namespace impl
--- GXTracking/GUTracking/src/Power2Divisor.cc
+++ GXTracking/GUTracking/src/Power2Divisor.cc
@@ -4,17 +4,41 @@ namespace vecphys {
 inline namespace VECPHYS_IMPL_NAMESPACE {
 
 VECPHYS_CUDA_HEADER_BOTH
-Power2Divisor::Power2Divisor(int nbins, int ndivs, Precision scale) 
-  : fNbins(nbins), fNdivs(ndivs), fScale(scale)
+Power2Divisor::Power2Divisor(int nmin, int nmax, int ndiv) 
+  : fNmin(nmin), fNmax(nmax), fNdiv(ndiv)
 {
-  fBinPosition = new Precision [fNbins];
+  fLowerBound = ldexp(1.,fNmin);
+  fUpperBound = ldexp(1.,fNmax);
+}
 
-  for(int i = 0 ; i < fNbins ; ++i) {
-    fBinPosition[i] = ldexp((1.+((i-1)%fNdivs)/(fNdivs)),int((i-1)/fNdivs));
-  }
+// following methods can be templated if necessary
 
-  fLowerBound = fScale;
-  fUpperBound = fScale*fBinPosition[fNbins-1];//GetBinPosition<kScalar>(fNbins);
+VECPHYS_CUDA_HEADER_BOTH
+int Power2Divisor::GetNumberOfBins()
+{
+  //check for fNmax > fNmin
+  return (fNmax-fNmin)*fNdiv+1;
+}
+
+VECPHYS_CUDA_HEADER_BOTH
+Precision Power2Divisor::GetBinPosition(int ibin)
+{
+  int exponent = Power2Exponent(ibin);
+  //  int idiv = ibin & (fNdiv -1); // idiv=ibin%ndiv for any fNdiv = 2^n
+  int idiv = ibin - fNdiv*Floor(ibin/fNdiv); //idiv=ibin%fNdiv (not restricted)
+  return  ldexp(1.+ 1.*idiv/fNdiv,exponent); 
+}
+
+VECPHYS_CUDA_HEADER_BOTH
+Precision Power2Divisor::GetBinSize(int ibin)
+{
+  int exponent = Power2Exponent(ibin);
+  return ldexp(1.,exponent)/fNdiv;
+}
+
+VECPHYS_CUDA_HEADER_BOTH
+int Power2Divisor::Power2Exponent(int ibin) {
+  return fNmin + ibin/fNdiv;
 }
 
 } // end namespace impl
