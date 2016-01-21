c445ad1548057c80573a7e7db1a506e79fe44dc3
Oksana Shadura <oksana.shadura@cern.ch>
2016-01-21 13:36:09 +0100

    [GEANT-183] FindPythia8.cmake is now supporting different version of Pythia8

--- cmake/modules/FindPythia8.cmake
+++ cmake/modules/FindPythia8.cmake
@@ -1,3 +1,4 @@
+# - Locate pythia8 libraries and includes
 # Defines:
 #
 #  PYTHIA8_FOUND
@@ -10,17 +11,30 @@
 #  PYTHIA8_LIBRARIES (not cached) : for PYTHIA8_VERSION < 200 includes 3 libraries above; not to be used if lhapdf is used
 
 
-find_path(PYTHIA8_INCLUDE_DIR Pythia8/Pythia.h
-PATHS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include)
+find_path(PYTHIA8_INCLUDE_DIR Pythia.h Pythia8/Pythia.h
+          HINTS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include)
+find_path(PYTHIA8_XML_DIR Version.xml
+          HINTS $ENV{PYTHIA8_ROOT_DIR}/xmldoc ${PYTHIA8_ROOT_DIR}/xmldoc $ENV{PYTHIA8_ROOT_DIR}/share/Pythia8/xmldoc ${PYTHIA8_ROOT_DIR}/share/Pythia8/xmldoc)
 
-find_library(PYTHIA8_LIBRARY NAMES pythia8
-PATHS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+message(STATUS "xml path: ${PYTHIA8_XML_DIR}")
 
-find_library(PYTHIA8_lhapdfdummy_LIBRARY NAMES lhapdfdummy
-PATHS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+#file(READ ${PYTHIA8_INCLUDE_DIR}/../xmldoc/Version.xml versionstr)
+file(READ ${PYTHIA8_XML_DIR}/Version.xml versionstr)
+string(REGEX REPLACE ".*Pythia:versionNumber.*default.*[0-9][.]([0-9]+).*" "\\1" PYTHIA8_VERSION "${versionstr}")
 
-set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${PYTHIA8_lhapdfdummy_LIBRARY})
+message(STATUS "pythia8 version extracted: ${PYTHIA8_VERSION}")
 
+find_library(PYTHIA8_LIBRARY NAMES pythia8 Pythia8
+             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+if(PYTHIA8_VERSION VERSION_LESS 200)
+  find_library(PYTHIA8_lhapdfdummy_LIBRARY NAMES lhapdfdummy
+               HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+  set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${PYTHIA8_lhapdfdummy_LIBRARY})
+else()
+  set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY})
+endif()
 
 set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR} ${PYTHIA8_INCLUDE_DIR}/Pythia8 )
 
@@ -28,5 +42,10 @@ set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR} ${PYTHIA8_INCLUDE_DIR}/Pythia8 )
 # all listed variables are TRUE
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
-mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+if(PYTHIA8_VERSION VERSION_LESS 200)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+  mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY  PYTHIA8_lhapdfdummy_LIBRARY PYTHIA8_VERSION_CHECK)
+else()
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY)
+  mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_VERSION_CHECK)
+endif()
