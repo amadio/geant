6a96e58b0700818e485ba6567f0b8a87dfec57fa
Andrei Gheata <andrei.gheata@cern.ch>
2016-03-17 11:00:45 -0500

    [GEANT-201] clang-format

--- vecprot_v2/inc/Basketizer.h
+++ vecprot_v2/inc/Basketizer.h
@@ -1,12 +1,12 @@
 //===--- Basketizer.h - Geant-V ---------------------------------*- C++ -*-===//
 //
-//                     Geant-V Prototype               
+//                     Geant-V Prototype
 //
 //===----------------------------------------------------------------------===//
 /**
  * @file Basketizer.h
- * @brief Implementation of basketizer of T* pointers for Geant-V prototype 
- * @author Andrei Gheata 
+ * @brief Implementation of basketizer of T* pointers for Geant-V prototype
+ * @author Andrei Gheata
  */
 //===----------------------------------------------------------------------===//
 #ifndef GEANT_BASKETIZER
@@ -20,7 +20,7 @@
 
 /**
  * @brief Class Basketizer
- * 
+ *
  * @param buffer_size Buffer size for queue
  * @tparam T Type of objects
  */
@@ -33,24 +33,26 @@ public:
   /**
    * @brief Basketizer dummy constructor
    */
-  Basketizer() : fBsize(0), fBmask(0), fLock(ATOMIC_FLAG_INIT), fBuffer(0),
-        fNadded(0), fBufferMask(0),
-        fIbook(0), fLow(0), fNstored(0), fNbaskets(0) {}
+  Basketizer()
+      : fBsize(0), fBmask(0), fLock(ATOMIC_FLAG_INIT), fBuffer(0), fNadded(0), fBufferMask(0), fIbook(0), fLow(0),
+        fNstored(0), fNbaskets(0) {}
   /**
    * @brief Basketizer default constructor
-   * 
+   *
    * @param buffer_size Circular buffer size
    * @param basket_size Size of produced baskets
    */
   Basketizer(size_t buffer_size, unsigned int basket_size)
-      : fBsize(0), fBmask(0), fLock(ATOMIC_FLAG_INIT), fBuffer(0),
-        fNadded(0), fBufferMask(0),
-        fIbook(0), fLow(0), fNstored(0), fNbaskets(0) {
+      : fBsize(0), fBmask(0), fLock(ATOMIC_FLAG_INIT), fBuffer(0), fNadded(0), fBufferMask(0), fIbook(0), fLow(0),
+        fNstored(0), fNbaskets(0) {
     Init(buffer_size, basket_size);
   }
 
   /** @brief Basketizer destructor */
-  ~Basketizer() { delete[] fBuffer; delete [] fNadded;}
+  ~Basketizer() {
+    delete[] fBuffer;
+    delete[] fNadded;
+  }
 
   //____________________________________________________________________________
   /** @brief Initialize basketizer */
@@ -59,12 +61,12 @@ public:
     assert((buffer_size >= 2) && ((buffer_size & (buffer_size - 1)) == 0));
     assert((basket_size >= 2) && ((basket_size & (basket_size - 1)) == 0));
     fBsize = basket_size;
-    fBuffer = new T*[buffer_size];
+    fBuffer = new T *[buffer_size];
     fNadded = new std::atomic<unsigned int>[buffer_size];
     fBufferMask = buffer_size - 1;
     fBsize = basket_size;
     fBmask = ~(fBsize - 1);
-    for (size_t i=0; i<buffer_size; ++i) {
+    for (size_t i = 0; i < buffer_size; ++i) {
       fBuffer[i] = nullptr;
       fNadded[i].store(0, std::memory_order_relaxed);
     }
@@ -73,26 +75,27 @@ public:
   //____________________________________________________________________________
   /** @brief Add an element to the basketizer */
   GEANT_INLINE
-  bool AddElement(T* const data, std::vector<T*> &basket) {
+  bool AddElement(T *const data, std::vector<T *> &basket) {
     // Book atomically a slot for copying the element
-//    size_t low = fLow.load(std::memory_order_relaxed);
+    //    size_t low = fLow.load(std::memory_order_relaxed);
     size_t ibook = fIbook.fetch_add(1);
     // We should check that we do not overwrite data here...
-//    assert(ibook-low < fBufferMask);
+    //    assert(ibook-low < fBufferMask);
     // Copy data at the booked location in the buffer
     size_t ibook_buf = ibook & fBufferMask;
     fBuffer[ibook_buf] = data;
     fNstored++;
-    
+
     // Get basket address for the booked index. Assume fixed size baskets.
     size_t buf_start = ibook_buf & fBmask;
     unsigned int nadded = fNadded[buf_start].fetch_add(1) + 1;
-    if (nadded == 1) fNbaskets++;
+    if (nadded == 1)
+      fNbaskets++;
     if (nadded >= fBsize) {
       // Deploy basket (copy data)
       fNstored -= fBsize;
       basket.clear();
-      basket.insert(basket.end(), &fBuffer[buf_start], &fBuffer[buf_start+fBsize]);
+      basket.insert(basket.end(), &fBuffer[buf_start], &fBuffer[buf_start + fBsize]);
       fNadded[buf_start].store(0, std::memory_order_release);
       fLow += fBsize;
       fNbaskets--;
@@ -104,8 +107,8 @@ public:
   //____________________________________________________________________________
   /** @brief Garbage collect data */
   GEANT_INLINE
-  bool GarbageCollect(std::vector<T*> &basket) {
-  // Garbage collect all elements present in the container on the current basket
+  bool GarbageCollect(std::vector<T *> &basket) {
+    // Garbage collect all elements present in the container on the current basket
     Lock();
     // Get current index, then compute a safe location forward
     size_t ibook = fIbook.load(std::memory_order_acquire);
@@ -123,7 +126,7 @@ public:
     if (!nelem) {
       Unlock();
       return false;
-    }  
+    }
     while (fNadded[buf_start].load(std::memory_order_relaxed) < nelem)
       ;
     fNstored -= nelem;
@@ -140,12 +143,14 @@ public:
   //____________________________________________________________________________
   /** @brief Change dynamically basket size */
   GEANT_INLINE
-  bool SetBasketSize(unsigned int bsize, std::vector<T*> &/*basket*/) {
+  bool SetBasketSize(unsigned int bsize, std::vector<T *> & /*basket*/) {
     int shift = 0;
     size_t basket_size;
-    while (bsize >> (++shift + 1));
+    while (bsize >> (++shift + 1))
+      ;
     basket_size = 1 << shift;
-    if (basket_size == fBsize) return false;
+    if (basket_size == fBsize)
+      return false;
     // Do not allow any other garbage collections or changing of basket size
     // while doing this.
     Lock();
@@ -153,51 +158,49 @@ public:
     // concurrently by AddElement
     Unlock();
     return false;
-  }  
+  }
 
   //____________________________________________________________________________
   /** @brief GetNumber of pending baskets */
   GEANT_INLINE
   short int GetNpending() const { return fNbaskets.load(); }
-  
+
   //____________________________________________________________________________
   /** @brief Lock the container for GC */
   GEANT_INLINE
   void Lock() {
     while (fLock.test_and_set(std::memory_order_acquire))
       ;
-  }    
+  }
 
   //____________________________________________________________________________
   /** @brief Unlock the container for GC */
   GEANT_INLINE
-  void Unlock() {
-    fLock.clear(std::memory_order_release);
-  }
-    
+  void Unlock() { fLock.clear(std::memory_order_release); }
+
 private:
   static const size_t cacheline_size = 64;
   typedef char cacheline_pad_t[cacheline_size];
-  size_t fBsize;                 /** Size of the produced baskets */
-  size_t fBmask;                 /** Basket mask */
-  std::atomic_flag fLock;        /** Lock for garbage collection and bsize changes */
+  size_t fBsize;          /** Size of the produced baskets */
+  size_t fBmask;          /** Basket mask */
+  std::atomic_flag fLock; /** Lock for garbage collection and bsize changes */
   // Make sure the following data members are not sitting in the same cache line
   cacheline_pad_t pad0_;
-  T ** fBuffer;                   /** Circular buffer for elements to be basketized*/
+  T **fBuffer; /** Circular buffer for elements to be basketized*/
   cacheline_pad_t pad1_;
   std::atomic<unsigned int> *fNadded; /** Circular buffer of atomic counters */
   cacheline_pad_t pad2_;
-  size_t fBufferMask;             /** Buffer mask used for fast index calculation in buffer */
+  size_t fBufferMask; /** Buffer mask used for fast index calculation in buffer */
   cacheline_pad_t pad3_;
-  std::atomic<size_t> fIbook;     /** Current booked index */
+  std::atomic<size_t> fIbook; /** Current booked index */
   cacheline_pad_t pad4_;
-  std::atomic<size_t> fLow;       /** Low watermark limit */
+  std::atomic<size_t> fLow; /** Low watermark limit */
   cacheline_pad_t pad5_;
-  std::atomic<size_t> fNstored;   /** Number of objects currently stored */
+  std::atomic<size_t> fNstored; /** Number of objects currently stored */
   cacheline_pad_t pad6_;
   std::atomic<short int> fNbaskets; /** Number of pending baskets */
   cacheline_pad_t pad7_;
-};  
+};
 } // Geant
 
 #endif
--- vecprot_v2/testBasketizer.cc
+++ vecprot_v2/testBasketizer.cc
@@ -11,79 +11,74 @@
 #endif
 
 struct test_track {
-  int    id_;
-  int    numa_;
-  int    type_;
+  int id_;
+  int numa_;
+  int type_;
   double x_;
   double y_;
   double z_;
-  double px_; 
+  double px_;
   double py_;
   double pz_;
   test_track() : id_(0), numa_(-1), type_(-1), x_(0.), y_(0.), z_(0.), px_(0.), py_(0.), pz_(0.) {}
 };
 
 //______________________________________________________________________________
-void help()
-{
-  std::cout << "Usage: testBasketizer <Nthreads>\n";
+void help() { std::cout << "Usage: testBasketizer <Nthreads>\n"; }
+
+//______________________________________________________________________________
+double get_wall_time() {
+  struct timeval time;
+  if (gettimeofday(&time, NULL)) {
+    //  Handle error
+    return 0;
+  }
+  return (double)time.tv_sec + (double)time.tv_usec * .000001;
 }
 
 //______________________________________________________________________________
-double get_wall_time(){
-    struct timeval time;
-    if (gettimeofday(&time,NULL)){
-        //  Handle error
-        return 0;
-    }
-    return (double)time.tv_sec + (double)time.tv_usec * .000001;
-}
+double get_cpu_time() { return (double)clock() / CLOCKS_PER_SEC; }
 
 //______________________________________________________________________________
-double get_cpu_time(){
-    return (double)clock() / CLOCKS_PER_SEC;
-}
-
-//______________________________________________________________________________
-void Process(std::vector<test_track*> &basket)
-{
-// Emulate CPU time on a basket
+void Process(std::vector<test_track *> &basket) {
+  // Emulate CPU time on a basket
   const int load = 35;
-  for (size_t itr=0; itr<basket.size(); ++itr) {
+  for (size_t itr = 0; itr < basket.size(); ++itr) {
     double x = 1.;
-    for (int j=0; j<100*load; ++j) {
-      x *= ((*basket[itr]).id_)%100;
+    for (int j = 0; j < 100 * load; ++j) {
+      x *= ((*basket[itr]).id_) % 100;
       double y = std::sqrt(x) * std::tanh(x);
-      x = std::sin(y)/std::atan(x);
+      x = std::sin(y) / std::atan(x);
     }
     (*basket[itr]).x_ = x;
   }
 }
 
-  using namespace Geant;
+using namespace Geant;
 //______________________________________________________________________________
 struct Workload {
 #ifdef USE_NUMA
-  NumaPolicy     policy_; // kSysDefault, kCompact, kScatter
+  NumaPolicy policy_; // kSysDefault, kCompact, kScatter
 #endif
-  size_t                nnodes_;
-  size_t                bsize_;
-  size_t                buf_size_;
-  size_t                nthreads_;
+  size_t nnodes_;
+  size_t bsize_;
+  size_t buf_size_;
+  size_t nthreads_;
   std::promise<size_t> *promise_;
-  std::future<size_t>  *future_;
-  size_t                checksum_ref_;
-  test_track           *tracks_;
-  size_t                ntracks_;
+  std::future<size_t> *future_;
+  size_t checksum_ref_;
+  test_track *tracks_;
+  size_t ntracks_;
   Basketizer<test_track> **basketizers_;
-  void                 *allocated_;
-  std::atomic_flag      fLock;
-  
-  Workload(size_t nthreads) : 
+  void *allocated_;
+  std::atomic_flag fLock;
+
+  Workload(size_t nthreads)
+      :
 #ifdef USE_NUMA
-      policy_(Geant::NumaPolicy::kCompact), 
+        policy_(Geant::NumaPolicy::kCompact),
 #endif
-      nnodes_(1), nthreads_(nthreads), fLock(ATOMIC_FLAG_INIT) {
+        nnodes_(1), nthreads_(nthreads), fLock(ATOMIC_FLAG_INIT) {
     using Basketizer = Geant::Basketizer<test_track>;
 #ifdef USE_NUMA
     std::cout << *policy_.GetTopology() << std::endl;
@@ -93,52 +88,51 @@ struct Workload {
     promise_ = new std::promise<size_t>[nthreads];
     future_ = new std::future<size_t>[nthreads];
     basketizers_ = new Basketizer *[nnodes_];
-    for (size_t n=0; n<nnodes_; ++n)
+    for (size_t n = 0; n < nnodes_; ++n)
       basketizers_[n] = nullptr;
-    for (size_t n = 0; n < nthreads; ++n) future_[n] = promise_[n].get_future();
+    for (size_t n = 0; n < nthreads; ++n)
+      future_[n] = promise_[n].get_future();
     ntracks_ = 1000000;
     const size_t nfilters = 1000;
     bsize_ = 32;
-    buf_size_ = 1<<10;
+    buf_size_ = 1 << 10;
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> rnd(0, nfilters-1);
-    // Create a pool of numbers  
+    std::uniform_int_distribution<> rnd(0, nfilters - 1);
+// Create a pool of numbers
 #ifdef USE_NUMA
-    allocated_ = numa_aligned_malloc(ntracks_*sizeof(test_track), 0 /*numa_node*/, 64);
+    allocated_ = numa_aligned_malloc(ntracks_ * sizeof(test_track), 0 /*numa_node*/, 64);
 #else
-    allocated_ = new char[ntracks_*sizeof(test_track)];
-#endif    
+    allocated_ = new char[ntracks_ * sizeof(test_track)];
+#endif
     tracks_ = new (allocated_) test_track[ntracks_];
 #ifdef USE_NUMA
     std::cout << "Allocated data " << allocated_ << " on node: " << numa_node_addr(allocated_) << std::endl;
 #endif
-    for (size_t i=0; i<ntracks_; ++i) {
+    for (size_t i = 0; i < ntracks_; ++i) {
       tracks_[i].id_ = i;
       // Generate randomly the filtering type
       tracks_[i].type_ = rnd(gen);
     }
-    checksum_ref_ = ntracks_ * (tracks_[0].id_ + tracks_[ntracks_-1].id_) / 2;
+    checksum_ref_ = ntracks_ * (tracks_[0].id_ + tracks_[ntracks_ - 1].id_) / 2;
   };
-  
+
   ~Workload() {
 #ifdef USE_NUMA
     numa_aligned_free(allocated_);
 #endif
-    for (size_t i=0; i<nnodes_; ++i)
+    for (size_t i = 0; i < nnodes_; ++i)
       delete basketizers_[i];
-    delete [] basketizers_;
+    delete[] basketizers_;
   }
 
   void Lock() {
     while (fLock.test_and_set(std::memory_order_acquire))
       ;
-  }    
-
-  void Unlock() {
-    fLock.clear(std::memory_order_release);
   }
-  
+
+  void Unlock() { fLock.clear(std::memory_order_release); }
+
   void InitBasketizers(size_t node) {
     // Create basketizers for each numa node. To be call by workers.
     using Basketizer = Geant::Basketizer<test_track>;
@@ -146,53 +140,53 @@ struct Workload {
     if (!basketizers_[node]) {
       basketizers_[node] = new Basketizer(buf_size_, bsize_);
 #ifdef USE_NUMA
-      std::cout << "basketizer[" << node << "] allocated on NUMA node " << numa_node_addr(basketizers_[node]) << std::endl;
+      std::cout << "basketizer[" << node << "] allocated on NUMA node " << numa_node_addr(basketizers_[node])
+                << std::endl;
 #endif
-    }  
-    Unlock();  
+    }
+    Unlock();
   }
-};  
+};
 
 //______________________________________________________________________________
-void AddTracks(int tid, Workload *work, size_t nchunk, size_t ntracks)
-{
-// Code run by each thread
+void AddTracks(int tid, Workload *work, size_t nchunk, size_t ntracks) {
+  // Code run by each thread
   // Pin thread according to the NUMA policy
   int numa_node = 0;
 #ifdef USE_NUMA
   numa_node = work->policy_.AllocateNextThread();
 #endif
-//  numa_node = (numa_node+1)%2;
+  //  numa_node = (numa_node+1)%2;
   work->InitBasketizers(numa_node);
   std::cout << "thread " << tid << " allocated on NUMA node: " << numa_node << std::endl;
-  test_track *tracks = &work->tracks_[tid*nchunk];
-  std::vector<test_track*> basket;
+  test_track *tracks = &work->tracks_[tid * nchunk];
+  std::vector<test_track *> basket;
   basket.reserve(work->bsize_);
   size_t checksum = 0;
-//  size_t checksum_ref = ntracks * (tracks[0].id_ + tracks[ntracks-1].id_) / 2;
-//  int npending, npmax=0;
-  for (size_t i=0; i<ntracks; ++i) {
-//    int type = tracks[i].type_;
+  //  size_t checksum_ref = ntracks * (tracks[0].id_ + tracks[ntracks-1].id_) / 2;
+  //  int npending, npmax=0;
+  for (size_t i = 0; i < ntracks; ++i) {
+    //    int type = tracks[i].type_;
     if (work->basketizers_[numa_node]->AddElement(&tracks[i], basket)) {
       // We got a basket = add to partial checksum
-      for (size_t itr=0; itr<basket.size(); ++itr) {
+      for (size_t itr = 0; itr < basket.size(); ++itr) {
         checksum += (*basket[itr]).id_;
-//        npending = basketizers[0].GetNpending();
-//        if (npending > npmax) npmax = npending;
-      }  
+        //        npending = basketizers[0].GetNpending();
+        //        if (npending > npmax) npmax = npending;
+      }
       Process(basket);
     }
   }
   if (work->basketizers_[numa_node]->GarbageCollect(basket)) {
-      for (size_t itr=0; itr<basket.size(); ++itr) {
-        checksum += (*basket[itr]).id_;
-        Process(basket);
-      }  
+    for (size_t itr = 0; itr < basket.size(); ++itr) {
+      checksum += (*basket[itr]).id_;
+      Process(basket);
+    }
   }
   work->promise_[tid].set_value(checksum);
-//  std::cout << "thread " << tid << " npmax: " << npmax << std::endl;
-//  std::cout << "thread " << tid << " checksum: " << checksum << " ref: " <<
-//            checksum_ref << std::endl;
+  //  std::cout << "thread " << tid << " npmax: " << npmax << std::endl;
+  //  std::cout << "thread " << tid << " checksum: " << checksum << " ref: " <<
+  //            checksum_ref << std::endl;
 }
 
 //______________________________________________________________________________
@@ -205,23 +199,25 @@ int main(int argc, char *argv[]) {
   }
   size_t nthreads = atoi(argv[1]);
   Workload work(nthreads);
-  size_t nchunk = work.ntracks_/nthreads;
+  size_t nchunk = work.ntracks_ / nthreads;
   std::vector<std::thread> v;
-  double cpu0  = get_cpu_time();  
-  double rt0 = get_wall_time(); 
+  double cpu0 = get_cpu_time();
+  double rt0 = get_wall_time();
   for (size_t n = 0; n < nthreads; ++n) {
     size_t ntoprocess = nchunk;
-    if (n == nthreads-1) ntoprocess = work.ntracks_-n*nchunk;
+    if (n == nthreads - 1)
+      ntoprocess = work.ntracks_ - n * nchunk;
     v.emplace_back(AddTracks, n, &work, nchunk, ntoprocess);
   }
-  for (auto& t : v) {
+  for (auto &t : v) {
     t.join();
   }
-  double rt1 = get_wall_time(); 
-  double cpu1  = get_cpu_time();
+  double rt1 = get_wall_time();
+  double cpu1 = get_cpu_time();
   size_t checksum = 0;
-  for (size_t n = 0; n < nthreads; ++n) checksum += work.future_[n].get();
+  for (size_t n = 0; n < nthreads; ++n)
+    checksum += work.future_[n].get();
 
-  std::cout << "run time: " << rt1-rt0 << "   cpu time: " << cpu1-cpu0 
-            << "  checksum: " << checksum << " ref: " << work.checksum_ref_ << std::endl;
+  std::cout << "run time: " << rt1 - rt0 << "   cpu time: " << cpu1 - cpu0 << "  checksum: " << checksum
+            << " ref: " << work.checksum_ref_ << std::endl;
 }
