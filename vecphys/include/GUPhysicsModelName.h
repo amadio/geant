e5f89c82cf56a87b5e41626f01c4d8f2002e9ff0
Guilherme Amadio <amadio@ncc.unesp.br>
2016-04-05 12:46:50 -0300

    Add inline implementation namespace to GUPhysicsModelName

--- vecphys/include/GUPhysicsModelName.h
+++ vecphys/include/GUPhysicsModelName.h
@@ -2,28 +2,29 @@
 #define GUPhysicsModelName_H
 
 namespace vecphys {
+inline namespace VECPHYS_IMPL_NAMESPACE {
 
 enum GUPhysicsModelIndex {
   kNullModel = -1,
   kKleinNishina,      // Compton
   kHybridKleinNishina,// Compton with Alias + Composition and Rejection
   kBetheHeitler,      // Conversion
-  kSauterGavrila,     // Photo-Electric Effect
   kMollerBhabha,      // Ionization
+  kSauterGavrila,     // Photo-Electric Effect
   kSeltzerBerger,     // Bremsstrahlung
   kNumberPhysicsModel
 };
 
-static
-const char* GUPhysicsModelName[kNumberPhysicsModel] = {
+static const char* GUPhysicsModelName[kNumberPhysicsModel] = {
   "KleinNishina",
   "HybridKleinNishina",
   "BetheHeitler",
-  "SauterGavrila",
   "MollerBhabha",
+  "SauterGavrila",
   "SeltzerBerger"
 };
 
+}
 } // end namespace vecphys
 
 #endif
