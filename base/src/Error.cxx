5b0f15b77647ea7969c3292174922600eca7c754
Philippe Canal <pcanal@fnal.gov>
2016-05-19 09:38:51 +0200

    GEANT-102 basic alternative implementation of error message handler

--- base/src/Error.cxx
+++ base/src/Error.cxx
@@ -1,13 +1,16 @@
 #include "Geant/Error.h"
+#ifdef USE_ROOT
 #include "TError.h"
-#include <stdarg.h>
 #include "Varargs.h"
+#endif
+#include <stdarg.h>
 
 namespace Geant {
 inline namespace cxx {
 
 // Code to be compiled only by gcc (i.e. not nvcc).
 
+#ifdef USE_ROOT
 void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *va_(fmt), ...)
 {
     // Currently we use the ROOT message handler on the host/gcc code.
@@ -17,6 +20,13 @@ void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *va_(fmt
    ::ErrorHandler((int)level,location, va_(fmt), ap);
    va_end(ap);
 }
+#else
+void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *msgfmt, ...)
+{
+   // Trivial implementation
+   vfprintf(stdout,"Geant Message level %d at %s: %s\n",level,location,msgfmt);
+}
+#endif
 
 } // cxx
 } // Geant
