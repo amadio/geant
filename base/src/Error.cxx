f36fc90768aa47aaffb20b8905ee4bec8c9ec908
Philippe Canal <pcanal@fnal.gov>
2016-07-13 14:28:50 -0500

    Reduce boiler plate duplication

--- base/src/Error.cxx
+++ base/src/Error.cxx
@@ -7,33 +7,26 @@
 namespace Geant {
 inline namespace cxx {
 
-// Code to be compiled only by gcc (i.e. not nvcc).
 
-#ifdef USE_ROOT
-void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *fmt, ...)
-{
-    // Currently we use the ROOT message handler on the host/gcc code.
-
-   va_list ap;
-   va_start(ap,fmt);
-   ::ErrorHandler((int)level,location, fmt, ap);
-   va_end(ap);
-}
-#else
 void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *msgfmt, ...)
 {
-   // Trivial implementation
    va_list args;
    va_start(args, msgfmt);
 
+#ifdef USE_ROOT
+    // Currently we use the ROOT message handler on the host/gcc code.
+   ::ErrorHandler((int)level,location, msgfmt, args);
+#else
+   // Trivial implementation
    if (level > EMsgLevel::kPrint || (location==nullptr || location[0]=='\0')) {
      fprintf(stdout,"Geant Message level %d at %s:",(int)level,location);
    }
    vfprintf(stdout, msgfmt, args);
    fprintf(stdout, "\n");
+#endif
+
    va_end(args);
 }
-#endif
 
 } // cxx
 } // Geant
