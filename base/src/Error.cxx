4af9115dca487ee9c51259856de66f065ec5885a
Philippe Canal <pcanal@fnal.gov>
2016-07-13 14:10:16 -0500

    We do not support Solaris, so no need for a layer of indirection in using va_start/va_end

--- base/src/Error.cxx
+++ base/src/Error.cxx
@@ -1,7 +1,6 @@
 #include "Geant/Error.h"
 #ifdef USE_ROOT
 #include "TError.h"
-#include "Varargs.h"
 #endif
 #include <stdarg.h>
 
@@ -11,28 +10,28 @@ inline namespace cxx {
 // Code to be compiled only by gcc (i.e. not nvcc).
 
 #ifdef USE_ROOT
-void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *va_(fmt), ...)
+void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *fmt, ...)
 {
     // Currently we use the ROOT message handler on the host/gcc code.
 
    va_list ap;
-   va_start(ap,va_(fmt));
-   ::ErrorHandler((int)level,location, va_(fmt), ap);
+   va_start(ap,fmt);
+   ::ErrorHandler((int)level,location, fmt, ap);
    va_end(ap);
 }
 #else
-void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *va_(msgfmt), ...)
+void ErrorHandlerImpl(EMsgLevel level, const char *location, const char *msgfmt, ...)
 {
    // Trivial implementation
    va_list args;
-   va_start(args, va_(msgfmt));
+   va_start(args, msgfmt);
 
    if (level > EMsgLevel::kPrint || (location==nullptr || location[0]=='\0')) {
-    vfprintf(stdout,"Geant Message level %d at %s:",level,location);
+     fprintf(stdout,"Geant Message level %d at %s:",(int)level,location);
    }
    vfprintf(stdout, msgfmt, args);
-   vfprintf(stdout, "\n");
-   va_end(ap);
+   fprintf(stdout, "\n");
+   va_end(args);
 }
 #endif
 
