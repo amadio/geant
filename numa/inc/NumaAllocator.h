ba307c2b70aaf60305a4503622bb245e1efb9897
Andrei Gheata <Andrei.Gheata@cern.ch>
2016-07-19 15:35:20 +0200

    [GEANT-201] Added missing NumaAlocator.h

---
+++ numa/inc/NumaAllocator.h
@@ -0,0 +1,69 @@
+//===--- NumaUtils.h - Geant-V ---------------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype               
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file NumaUtils.h
+ * @brief Utilities for NUMA management
+ * @author Andrei Gheata 
+ */
+//===----------------------------------------------------------------------===//
+#ifndef GEANT_NUMA_ALLOCATOR
+#define GEANT_NUMA_ALLOCATOR
+
+#include <iostream>
+
+#ifndef GEANT_NUMA_UTILS
+#include "NumaUtils.h"
+#endif
+
+/**
+  NUMA-aware allocator following std::alocator requirements.
+  Use with any type as: 
+    std::vector<T, NumaAllocator<T>> basket(init_size, init_value, NumaAllocator<T>(numafNode));
+*/
+
+namespace Geant {
+inline namespace GEANT_IMPL_NAMESPACE {
+
+  template <typename T>
+  class NumaAllocator: public std::allocator<T> {
+    private:
+    int fNode; /** Numa node */
+
+    public:
+    typedef size_t size_type;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+  
+    template<typename U>
+    struct rebind {
+      typedef NumaAllocator<U> other;
+    };
+
+    pointer allocate(size_type n) {
+      // std::cout << "Alloc " << n*sizeof(T) << " bytes on numa node " << fNode << std::endl;
+      return static_cast<pointer>(NumaAlignedMalloc(n*sizeof(T), fNode, 64));
+      //return std::allocator<T>::allocate(n, hint);
+    }
+
+    void deallocate(pointer p, size_type /*n*/) {
+      // std::cout <<  "Dealloc " <<  n*sizeof(T) << " bytes.\n";
+      NumaAlignedFree(p);
+      //return std::allocator<T>::deallocate(p, n);
+    }
+
+    NumaAllocator(int node) throw(): std::allocator<T>(), fNode(node) { }
+    NumaAllocator(const NumaAllocator &a) throw(): std::allocator<T>(a), fNode(a.fNode) { }
+
+    template <class V>
+    NumaAllocator(const NumaAllocator<V> &a) throw(): std::allocator<T>(a), fNode(a.fNode) { }
+
+    ~NumaAllocator() throw() { }
+  };
+
+} // GEANT_IMPL_NAMESPACE
+} // Geant
+
+#endif
