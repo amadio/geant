08c28bb3a4025fc6d37aeff91735c2dd3d5a5fde
Andrei Gheata <Andrei.Gheata@cern.ch>
2016-07-19 15:21:18 +0200

    [GEANT-201] Fixed compilation error in TGeo mode. Added Findhwloc.cmake

---
+++ cmake/modules/Findhwloc.cmake
@@ -0,0 +1,51 @@
+# Module for locating libhwloc
+#
+# Read-only variables:
+#   hwloc_FOUND
+#     Indicates that the library has been found.
+#
+#   HWLOC_LIBRARIES
+#     Points to the d
+#     The content of this variable can be passed to target_link_libraries.
+#
+
+# Use pkg-config to fetch the contents of the .pc file
+if(NOT PKGCONFIG_FOUND)
+  find_package(PkgConfig QUIET)
+endif()
+
+if(HWLOC_ROOT)
+  set(ENV{PKG_CONFIG_PATH} "${HWLOC_ROOT}/lib/pkgconfig")
+else()
+  foreach(PREFIX ${CMAKE_PREFIX_PATH})
+    set(PKG_CONFIG_PATH "${PKG_CONFIG_PATH}:${PREFIX}/lib/pkgconfig")
+  endforeach()
+  set(ENV{PKG_CONFIG_PATH} "${PKG_CONFIG_PATH}:$ENV{PKG_CONFIG_PATH}")
+endif()
+
+if(hwloc_FIND_REQUIRED)
+  set(_hwloc_OPTS "REQUIRED")
+elseif(hwloc_FIND_QUIETLY)
+  set(_hwloc_OPTS "QUIET")
+else()
+  set(_hwloc_output 1)
+endif()
+
+if(hwloc_FIND_VERSION)
+  if(hwloc_FIND_VERSION_EXACT)
+    pkg_check_modules(HWLOC ${_hwloc_OPTS} hwloc=${hwloc_FIND_VERSION})
+  else()
+    pkg_check_modules(HWLOC ${_hwloc_OPTS} hwloc>=${hwloc_FIND_VERSION})
+  endif()
+else()
+  pkg_check_modules(HWLOC ${_hwloc_OPTS} hwloc)
+endif()
+
+if(hwloc_FOUND)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(HWLOC DEFAULT_MSG HWLOC_LIBRARIES)
+
+  message(STATUS
+    "Found hwloc ${hwloc_VERSION} library: ${HWLOC_LIBRARIES}")
+  set (HWLOC_FOUND true)
+endif()
--- vecprot_v2/src/GeantTrack.cxx
+++ vecprot_v2/src/GeantTrack.cxx
@@ -261,10 +261,10 @@ void GeantTrack::Print(const char *msg) const {
   TString nextpath;
   fNextpath->GetPath(nextpath);
 
-  Geant::Print(msg, "== Track %d: evt=%d slt=%d part=%d mth=%d pdg=%d gvc=%d eind=%d bind=%d chg=%d proc=%d nstp=%d "
+  Geant::Print(msg, ": evt=%d slt=%d part=%d mth=%d pdg=%d gvc=%d eind=%d bind=%d chg=%d proc=%d nstp=%d "
          "spc=%d status=%s mass=%g xpos=%g ypos=%g zpos=%g xdir=%g ydir=%g zdir=%g mom=%g ene=%g "
          "time=%g edep=%g pstp=%g stp=%g snxt=%g saf=%g nil=%g ile=%g bdr=%d\n pth=%s npth=%s\n",
-         itr, fEvent, fEvslot, fParticle, fMother, fPDG, fGVcode, fEindex, fBindex,
+         fEvent, fEvslot, fParticle, fMother, fPDG, fGVcode, fEindex, fBindex,
          fCharge, fProcess, fNsteps, (int)fSpecies, status[int(fStatus)],
          fMass, fXpos, fYpos, fZpos, fXdir, fYdir, fZdir, fP, fE,
          fTime, fEdep, fPstep, fStep, fSnext, fSafety, fNintLen, fIntLen, fBoundary, path.Data(),
