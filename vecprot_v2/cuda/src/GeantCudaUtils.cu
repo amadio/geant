e80e8d6cf3a0e33d0abcc29db6b5d9958172b84d
Federico Carminati <federico.carminati@cern.ch>
2016-08-04 11:48:53 +0200

    [GEANT-225]Enable use of USE_ROOT=OFF

--- vecprot_v2/cuda/src/GeantCudaUtils.cu
+++ vecprot_v2/cuda/src/GeantCudaUtils.cu
@@ -3,7 +3,9 @@
 #include "GeantCudaUtils.h"
 #include "GeantPropagator.h"
 #include "GeantTrack.h"
+#ifdef USE_ROOT
 #include "TGeoShape.h"
+#endif
 #include "globals.h"
 
 namespace Geant {
@@ -14,7 +16,11 @@ void CoprocessorBrokerInitConstant()
   GEANT_CUDA_ERROR(
       cudaMemcpyToSymbol(gPropagator_fBmag, &(gPropagator->fBmag), sizeof(double), size_t(0), cudaMemcpyHostToDevice));
 
+#ifdef USE_ROOT
   double tolerance = TGeoShape::Tolerance();
+#else
+  double tolerance = 1e-7;
+#endif
   GEANT_CUDA_ERROR(
       cudaMemcpyToSymbol(device_constant::gTolerance, &(tolerance), sizeof(double), size_t(0), cudaMemcpyHostToDevice));
 }
