1d3a6ef89e3bdcaff5f74ac3beb3dfd2ac98f3ff
Soon Yung Jun <syjun@mic.fnal.gov>
2016-11-15 16:49:53 -0600

    [GEANT-238] instantiate models correctly for tests with composition and rejection methods

--- vecphys/src/IonisationMoller.cc
+++ vecphys/src/IonisationMoller.cc
@@ -51,23 +51,23 @@ VECCORE_ATT_HOST void IonisationMoller::BuildPdfTable(int Z, double *p)
   const int ncol = fAliasSampler->GetSamplesPerEntry();
 
   double logxmin = math::Log(fAliasSampler->GetIncomingMin());
-  double dx = (math::Log(fAliasSampler->GetIncomingMax()) - logxmin) / nrow;
+  double dx      = (math::Log(fAliasSampler->GetIncomingMax()) - logxmin) / nrow;
 
   for (int i = 0; i <= nrow; ++i) {
     // for each input energy bin
     double x = math::Exp(logxmin + dx * i);
 
     // e-e- (Moller) only for now
-    double ymin = fDeltaRayThreshold;    // minimum delta-ray energy
-    double dy = (x / 2.0 - ymin) / ncol; // maximum x/2.0
-    double yo = ymin + 0.5 * dy;
+    double ymin = fDeltaRayThreshold;      // minimum delta-ray energy
+    double dy   = (x / 2.0 - ymin) / ncol; // maximum x/2.0
+    double yo   = ymin + 0.5 * dy;
 
     double sum = 0.;
 
     for (int j = 0; j < ncol; ++j) {
       // for each output energy bin
-      double y = yo + dy * j;
-      double xsec = CalculateDiffCrossSection(Z, x, y);
+      double y        = yo + dy * j;
+      double xsec     = CalculateDiffCrossSection(Z, x, y);
       p[i * ncol + j] = xsec;
       sum += xsec;
     }
@@ -84,7 +84,7 @@ VECCORE_ATT_HOST void IonisationMoller::BuildPdfTable(int Z, double *p)
 // function implementing the cross section for MollerBhabha
 
 VECCORE_ATT_HOST_DEVICE double IonisationMoller::CalculateDiffCrossSection(int /*Zelement*/, double kineticEnergy,
-                                                                            double deltaRayEnergy) const
+                                                                           double deltaRayEnergy) const
 {
   // based on Geant3 : Simulation of the delta-ray production (PHY331-1)
   // input  : kineticEnergy (incomming photon energy)
@@ -93,8 +93,8 @@ VECCORE_ATT_HOST_DEVICE double IonisationMoller::CalculateDiffCrossSection(int /
 
   double dcross = 0.0;
 
-  double tau = kineticEnergy / electron_mass_c2;
-  double gam = tau + 1.0;
+  double tau    = kineticEnergy / electron_mass_c2;
+  double gam    = tau + 1.0;
   double gamma2 = gam * gam;
 
   double epsil = deltaRayEnergy / kineticEnergy;
@@ -103,8 +103,8 @@ VECCORE_ATT_HOST_DEVICE double IonisationMoller::CalculateDiffCrossSection(int /
   // Bhabha (e+e-) scattering not implemented for now
 
   double fgam = (2 * gam - 1.0) / gamma2;
-  double x = 1 / epsil;
-  double y = 1 / (1.0 - epsil);
+  double x    = 1 / epsil;
+  double y    = 1 / (1.0 - epsil);
 
   dcross = 1 - fgam + x * (x - fgam) + y * (y - fgam);
 
@@ -120,20 +120,20 @@ VECCORE_ATT_HOST double IonisationMoller::GetG4CrossSection(int Z, double kineti
   double maxEnergy = 1.0 * TeV;
 
   double tmax = 0.5 * kineticEnergy;
-  tmax = math::Min(maxEnergy, tmax);
+  tmax        = math::Min(maxEnergy, tmax);
 
   if (cutEnergy < tmax) {
-    double xmin = cutEnergy / kineticEnergy;
-    double xmax = tmax / kineticEnergy;
-    double tau = kineticEnergy / electron_mass_c2;
-    double gam = tau + 1.0;
+    double xmin   = cutEnergy / kineticEnergy;
+    double xmax   = tmax / kineticEnergy;
+    double tau    = kineticEnergy / electron_mass_c2;
+    double gam    = tau + 1.0;
     double gamma2 = gam * gam;
-    double beta2 = tau * (tau + 2) / gamma2;
+    double beta2  = tau * (tau + 2) / gamma2;
 
     // Moller (e-e-) scattering
 
     double gg = (2.0 * gam - 1.0) / gamma2;
-    cross = ((xmax - xmin) * (1.0 - gg + 1.0 / (xmin * xmax) + 1.0 / ((1.0 - xmin) * (1.0 - xmax))) -
+    cross     = ((xmax - xmin) * (1.0 - gg + 1.0 / (xmin * xmax) + 1.0 / ((1.0 - xmin) * (1.0 - xmax))) -
              gg * math::Log(xmax * (1.0 - xmin) / (xmin * (1.0 - xmax)))) /
             beta2;
   }
@@ -143,8 +143,8 @@ VECCORE_ATT_HOST double IonisationMoller::GetG4CrossSection(int Z, double kineti
 }
 
 VECCORE_ATT_HOST_DEVICE void IonisationMoller::SampleByCompositionRejection(int /*Z*/, // not used
-                                                                             double kineticEnergy,
-                                                                             double &deltaKinEnergy, double &sinTheta)
+                                                                            double kineticEnergy,
+                                                                            double &deltaKinEnergy, double &sinTheta)
 {
   // temporary - set by material
   double cutEnergy = fDeltaRayThreshold;
@@ -164,17 +164,17 @@ VECCORE_ATT_HOST_DEVICE void IonisationMoller::SampleByCompositionRejection(int 
   }
 
   double energy = kineticEnergy + electron_mass_c2;
-  double xmin = tmin / kineticEnergy;
-  double xmax = tmax / kineticEnergy;
-  double gam = energy / electron_mass_c2;
+  double xmin   = tmin / kineticEnergy;
+  double xmax   = tmax / kineticEnergy;
+  double gam    = energy / electron_mass_c2;
   double gamma2 = gam * gam;
   //  double beta2  = 1.0 - 1.0/gamma2;
   double x, z, q, grej;
 
   // Moller (e-e-) scattering
   double gg = (2.0 * gam - 1.0) / gamma2;
-  double y = 1.0 - xmax;
-  grej = 1.0 - gg * xmax + xmax * xmax * (1.0 - gg + (1.0 - gg * y) / (y * y));
+  double y  = 1.0 - xmax;
+  grej      = 1.0 - gg * xmax + xmax * xmax * (1.0 - gg + (1.0 - gg * y) / (y * y));
 
   do {
     q = UniformRandom<double>(fRandomState, fThreadId);
@@ -188,7 +188,7 @@ VECCORE_ATT_HOST_DEVICE void IonisationMoller::SampleByCompositionRejection(int 
   double totalMomentum = math::Sqrt(kineticEnergy * (kineticEnergy + 2.0 * electron_mass_c2));
 
   double deltaMomentum = math::Sqrt(deltaKinEnergy * (deltaKinEnergy + 2.0 * electron_mass_c2));
-  double cost = deltaKinEnergy * (energy + electron_mass_c2) / (deltaMomentum * totalMomentum);
+  double cost          = deltaKinEnergy * (energy + electron_mass_c2) / (deltaMomentum * totalMomentum);
   if (cost > 1.0) {
     cost = 1.0;
   }
