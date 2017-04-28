f981844732cf90d4c65a0ab0b1ee9dd46fbda393
Philippe Canal <pcanal@fnal.gov>
2017-04-28 13:11:45 -0500

    Use override keyword and remove redundant virtual keyword

--- xsec/inc/TestProcess.h
+++ xsec/inc/TestProcess.h
@@ -26,39 +26,35 @@ class TestProcess : public Geant::PhysicsProcessOld {
     TestProcess();
     virtual ~TestProcess() {}
 
-    virtual void Initialize();
-    virtual void ComputeIntLen( Material_t *mat, int ntracks, GeantTrack_v &tracks,
-                                GeantTaskData *td );
-
-    // dummy method: PostStep has been splitted up into two parts (see below)
-    virtual void PostStep( Material_t * /*mat*/, int /*ntracks*/, GeantTrack_v & /*tracks */, int & /*nout*/,
-                           GeantTaskData * /*td*/ ) {}
+    void Initialize() override;
+    void ComputeIntLen( Material_t *mat, int ntracks, GeantTrack_v &tracks,
+                        GeantTaskData *td ) override;
 
     // sampling: target atom and type of the interaction for each primary tracks;
     //           all inf. regarding sampling output is stored in the tracks
-    virtual void PostStepTypeOfIntrActSampling( Material_t *mat, int ntracks, GeantTrack_v &tracks,
-                                                GeantTaskData *td );
+    void PostStepTypeOfIntrActSampling( Material_t *mat, int ntracks, GeantTrack_v &tracks,
+                                        GeantTaskData *td ) override;
 
     // sampling final states for each primary tracks based on target atom and
     // interaction type sampled by PostStepTypeOfIntrActSampling;
     // updating primary track properties and inserting secondary tracks;
     // number of inserted secondary tracks will be stored in nout at termination
-    virtual void PostStepFinalStateSampling( Material_t *mat, int ntracks, GeantTrack_v &tracks, int &nout,
-                                             GeantTaskData *td );
+    void PostStepFinalStateSampling( Material_t *mat, int ntracks, GeantTrack_v &tracks, int &nout,
+                                     GeantTaskData *td ) override;
 
-    virtual void AtRest( int /*ntracks*/, GeantTrack_v &/* tracks */, int &/* nout */, GeantTaskData */* td */ ) {};
+    void AtRest( int /*ntracks*/, GeantTrack_v &/* tracks */, int &/* nout */, GeantTaskData */* td */ ) override {};
 
-    virtual void Eloss( Material_t */* mat */, int /* ntracks */, GeantTrack_v &/* tracks */, int &/* nout */, GeantTaskData */* td */ );
+    void Eloss( Material_t */* mat */, int /* ntracks */, GeantTrack_v &/* tracks */, int &/* nout */, GeantTaskData */* td */ ) override;
 
-    virtual void ApplyMsc( Material_t */* mat */, int /* ntracks */, GeantTrack_v &/* tracks */, GeantTaskData */* td */ ) {};
+    void ApplyMsc( Material_t */* mat */, int /* ntracks */, GeantTrack_v &/* tracks */, GeantTaskData */* td */ ) override {};
 
 //=== N E W   I N T E R F A C E S ===//
   VECCORE_ATT_HOST_DEVICE
-  virtual void ComputeIntLen(TrackVec_t &, GeantTaskData *) {}
+  virtual void ComputeIntLen(TrackVec_t &, GeantTaskData *) override {}
   VECCORE_ATT_HOST_DEVICE
-  virtual void PostStepTypeOfIntrActSampling(TrackVec_t &, GeantTaskData *) {}
+  virtual void PostStepTypeOfIntrActSampling(TrackVec_t &, GeantTaskData *) override {}
   VECCORE_ATT_HOST_DEVICE
-  virtual void PostStepFinalStateSampling(TrackVec_t &, int &, TrackVec_t &, GeantTaskData *) {}
+  virtual void PostStepFinalStateSampling(TrackVec_t &, int &, TrackVec_t &, GeantTaskData *) override {}
 
   private:
 
