a87eeccd0a4dde7e6da7f8caa41c039f6fdfb2d1
Witold Pokorski <witoldp@Witolds-MacBook-Pro-2.local>
2017-05-19 17:27:43 +0200

    fixing bug, charge and mass was swaped

--- realphysics/dummies/inc/Particle.h
+++ realphysics/dummies/inc/Particle.h
@@ -55,8 +55,8 @@ private:
   int     fIndex;        // in the global particle table
   int     fInternalCode;
   int     fPDGCode;
-  double  fPDGCharge;
   double  fPDGMass;
+  double  fPDGCharge;
 
   std::vector<PhysicsProcess*>  fPhysicsProcessVector;  // only one and used only as temporary storage
   std::vector<PhysicsManagerPerParticle*> fPMPParticle;  // as many as regions but those having no any active processes will be nullptr
