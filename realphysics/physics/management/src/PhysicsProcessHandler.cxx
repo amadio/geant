f7956b3ca51ae4f8d08a65a330d34c3acc687bc4
Mihaly Novak <mnovak@cern.ch>
2017-07-28 15:03:18 +0200

    GEANT-331 Remove forgotten debug setting.

--- realphysics/physics/management/src/PhysicsProcessHandler.cxx
+++ realphysics/physics/management/src/PhysicsProcessHandler.cxx
@@ -45,9 +45,6 @@ PhysicsProcessHandler::~PhysicsProcessHandler() {
 
 
 void PhysicsProcessHandler::Initialize() {
-  PhysicsParameters::SetDefaultGammaCutInLength(0.07);
-  PhysicsParameters::SetDefaultElectronCutInLength(0.07);
-  PhysicsParameters::SetDefaultPositronCutInLength(0.07);
   //
   // create all MaterialCuts
   MaterialCuts::CreateAll();
