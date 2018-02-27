24f34d757027bae6850a017d954672285615d335
Philippe Canal <pcanal@fnal.gov>
2018-02-27 17:27:24 -0600

    No longer copy header back into source directory

--- cmake/modules/GeantMacros.cmake
+++ cmake/modules/GeantMacros.cmake
@@ -1,20 +1,3 @@
-MACRO(add_headers headers)
-  set(allh)
-  foreach(_header ${headers})		     
-    GET_FILENAME_COMPONENT(_base_header ${_header} NAME)
-    add_custom_command(OUTPUT ${PROJECT_SOURCE_DIR}/include/${_base_header}
-    COMMAND cp ${_header} ${PROJECT_SOURCE_DIR}/include/${_base_header}
-    DEPENDS  ${PROJECT_SOURCE_DIR}/include ${_header})
-    set(allh ${allh} ${PROJECT_SOURCE_DIR}/include/${_base_header})
-  endforeach(_header)
-  add_custom_command(OUTPUT ${PROJECT_SOURCE_DIR}/include
-  		   COMMAND mkdir -p ${PROJECT_SOURCE_DIR}/include)
-  string(REPLACE ${PROJECT_SOURCE_DIR}/ "" _leaf ${CMAKE_CURRENT_SOURCE_DIR})
-  string(REPLACE "/" "_" _leaf ${_leaf})
-  add_custom_target(${_leaf}_headers DEPENDS ${PROJECT_SOURCE_DIR}/include ${allh})
-  add_dependencies(_headers ${_leaf}_headers)
-endmacro(add_headers)
-
  # define a function downloading from a URL into a local file LOCALFILE
  function(FILE_DOWNLOAD FILE_URL LOCALFILE )
   if(APPLE)
