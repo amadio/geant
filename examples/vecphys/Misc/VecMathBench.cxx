96fda23986c4c471e36493d1d848b3f168c15a21
Vitalii Drohan <vitalii.drohan@cern.ch>
2018-03-15 09:24:41 +0100

    Math bench added

---
+++ examples/vecphys/Misc/VecMathBench.cxx
@@ -0,0 +1,124 @@
+#include <benchmark/benchmark.h>
+
+#include "Geant/VecRngWrapper.h"
+
+const int kN = 256;
+
+static void ScalarExp(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+  std::vector<double> rnd;
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd.push_back(wrapper.uniform());
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0); ++i) {
+      sum += std::exp(rnd[i]);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(ScalarExp)->Arg(kN);
+
+static void VectorExp(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+
+  double *rnd = (double *)vecCore::AlignedAlloc(kPhysDVAlign, state.range(0) * sizeof(double));
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd[i] = wrapper.uniform();
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+      PhysDV tmp;
+      vecCore::Load(tmp, &rnd[i]);
+      tmp = vecCore::math::Exp(tmp);
+      sum += vecCore::ReduceAdd(tmp);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+  vecCore::AlignedFree(rnd);
+}
+BENCHMARK(VectorExp)->Arg(kN);
+
+static void ScalarLog(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+  std::vector<double> rnd;
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd.push_back(wrapper.uniform());
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0); ++i) {
+      sum += std::log(rnd[i]);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(ScalarLog)->Arg(kN);
+
+static void VectorLog(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+
+  double *rnd = (double *)vecCore::AlignedAlloc(kPhysDVAlign, state.range(0) * sizeof(double));
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd[i] = wrapper.uniform();
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+      PhysDV tmp;
+      vecCore::Load(tmp, &rnd[i]);
+      tmp = vecCore::math::Log(tmp);
+      sum += vecCore::ReduceAdd(tmp);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+  vecCore::AlignedFree(rnd);
+}
+BENCHMARK(VectorLog)->Arg(kN);
+
+static void ScalarSqrt(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+  std::vector<double> rnd;
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd.push_back(wrapper.uniform());
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0); ++i) {
+      sum += std::sqrt(rnd[i]);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(ScalarSqrt)->Arg(kN);
+
+static void VectorSqrt(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+
+  double *rnd = (double *)vecCore::AlignedAlloc(kPhysDVAlign, state.range(0) * sizeof(double));
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd[i] = wrapper.uniform();
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+      PhysDV tmp;
+      vecCore::Load(tmp, &rnd[i]);
+      tmp = vecCore::math::Sqrt(tmp);
+      sum += vecCore::ReduceAdd(tmp);
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+  vecCore::AlignedFree(rnd);
+}
+BENCHMARK(VectorSqrt)->Arg(kN);
+
+BENCHMARK_MAIN();
