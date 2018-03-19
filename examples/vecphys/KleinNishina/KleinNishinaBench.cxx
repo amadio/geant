48e7994f42883acf64d6b6a6df732a1010fc398b
Vitalii Drohan <vitalii.drohan@cern.ch>
2018-03-19 14:45:09 +0100

    Baseline measurements added

--- examples/vecphys/KleinNishina/KleinNishinaBench.cxx
+++ examples/vecphys/KleinNishina/KleinNishinaBench.cxx
@@ -3,6 +3,27 @@
 
 #include "Geant/VecRngWrapper.h"
 
+static void KleinNishinaScalarAliasBaseline(benchmark::State &state)
+{
+  KleinNishinaComptonModel *kNish = PrepareKnishinaModel(true);
+
+  auto Td = PrepareTaskData();
+  std::vector<LightTrack> primaries;
+
+  int basketSize = state.range(0);
+
+  for (auto _ : state) {
+    PreparePrimaries(primaries, basketSize);
+    Td->fPhysicsData->ClearSecondaries();
+  }
+
+  benchmark::DoNotOptimize(primaries.data());
+
+  delete kNish;
+  CleanTaskData(Td);
+}
+BENCHMARK(KleinNishinaScalarAliasBaseline)->RangeMultiplier(2)->Range(kMinBasket, kMaxBasket);
+
 static void KleinNishinaScalarAlias(benchmark::State &state)
 {
   KleinNishinaComptonModel *kNish = PrepareKnishinaModel(true);
@@ -13,9 +34,7 @@ static void KleinNishinaScalarAlias(benchmark::State &state)
   int basketSize = state.range(0);
 
   for (auto _ : state) {
-    state.PauseTiming();
     PreparePrimaries(primaries, basketSize);
-    state.ResumeTiming();
     Td->fPhysicsData->ClearSecondaries();
     for (int i = 0; i < basketSize; ++i) {
       kNish->SampleSecondaries(primaries[i], Td);
@@ -26,6 +45,29 @@ static void KleinNishinaScalarAlias(benchmark::State &state)
   CleanTaskData(Td);
 }
 BENCHMARK(KleinNishinaScalarAlias)->RangeMultiplier(2)->Range(kMinBasket, kMaxBasket);
+
+static void KleinNishinaVectorAliasBaseline(benchmark::State &state)
+{
+  VecKleinNishinaComptonModel *kNish = PrepareVecKnishinaModel(true);
+
+  auto Td = PrepareTaskData();
+
+  LightTrack_v primaries;
+
+  int basketSize = state.range(0);
+
+  for (auto _ : state) {
+    PreparePrimaries(primaries, basketSize);
+    primaries.SetNtracks(basketSize);
+
+    Td->fPhysicsData->GetSecondarySOA().ClearTracks();
+  }
+  benchmark::DoNotOptimize(&primaries);
+
+  delete kNish;
+  CleanTaskData(Td);
+}
+BENCHMARK(KleinNishinaVectorAliasBaseline)->RangeMultiplier(2)->Range(kMinBasket, kMaxBasket);
 
 static void KleinNishinaVectorAlias(benchmark::State &state)
 {
@@ -38,10 +80,8 @@ static void KleinNishinaVectorAlias(benchmark::State &state)
   int basketSize = state.range(0);
 
   for (auto _ : state) {
-    state.PauseTiming();
     PreparePrimaries(primaries, basketSize);
     primaries.SetNtracks(basketSize);
-    state.ResumeTiming();
 
     Td->fPhysicsData->GetSecondarySOA().ClearTracks();
     kNish->SampleSecondariesVector(primaries, Td);
