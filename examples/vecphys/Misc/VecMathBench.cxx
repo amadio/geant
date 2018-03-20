d5e49b1ddf619abd3fafe5eecc25237819a37701
Vitalii Drohan <vitalii.drohan@cern.ch>
2018-03-20 09:14:10 +0100

    Add div benchmark

--- examples/vecphys/Misc/VecMathBench.cxx
+++ examples/vecphys/Misc/VecMathBench.cxx
@@ -31,7 +31,7 @@ static void VectorExp(benchmark::State &state)
   }
   for (auto _ : state) {
     double sum = 0.0;
-    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+    for (int i = 0; i < state.range(0); i += kPhysDVWidth) {
       PhysDV tmp;
       vecCore::Load(tmp, &rnd[i]);
       tmp = vecCore::math::Exp(tmp);
@@ -70,7 +70,7 @@ static void VectorLog(benchmark::State &state)
   }
   for (auto _ : state) {
     double sum = 0.0;
-    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+    for (int i = 0; i < state.range(0); i += kPhysDVWidth) {
       PhysDV tmp;
       vecCore::Load(tmp, &rnd[i]);
       tmp = vecCore::math::Log(tmp);
@@ -109,7 +109,7 @@ static void VectorSqrt(benchmark::State &state)
   }
   for (auto _ : state) {
     double sum = 0.0;
-    for (int i = 0; i < state.range(0) / kPhysDVWidth; ++i) {
+    for (int i = 0; i < state.range(0); i += kPhysDVWidth) {
       PhysDV tmp;
       vecCore::Load(tmp, &rnd[i]);
       tmp = vecCore::math::Sqrt(tmp);
@@ -120,5 +120,52 @@ static void VectorSqrt(benchmark::State &state)
   vecCore::AlignedFree(rnd);
 }
 BENCHMARK(VectorSqrt)->Arg(kN);
+
+static void ScalarDiv(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+  std::vector<double> rnd;
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd.push_back(wrapper.uniform());
+  }
+  for (auto _ : state) {
+    double sum = 0.0;
+    for (int i = 0; i < state.range(0); ++i) {
+      double tmp = rnd[i];
+      tmp        = 1.0 / tmp;
+      tmp        = 1.0 / tmp;
+      tmp        = 1.0 / tmp;
+      tmp        = 1.0 / tmp;
+      sum += tmp;
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(ScalarDiv)->Arg(kN);
+
+static void VectorDiv(benchmark::State &state)
+{
+  geant::VecRngWrapper wrapper;
+
+  double *rnd = (double *)vecCore::AlignedAlloc(kPhysDVAlign, state.range(0) * sizeof(double));
+  for (int i = 0; i < state.range(0); ++i) {
+    rnd[i] = wrapper.uniform();
+  }
+  for (auto _ : state) {
+    PhysDV sum = 0.0;
+    for (int i = 0; i < state.range(0); i += kPhysDVWidth) {
+      PhysDV tmp;
+      vecCore::Load(tmp, &rnd[i]);
+      tmp = 1.0 / tmp;
+      tmp = 1.0 / tmp;
+      tmp = 1.0 / tmp;
+      tmp = 1.0 / tmp;
+      sum += tmp;
+    }
+    benchmark::DoNotOptimize(sum);
+  }
+  vecCore::AlignedFree(rnd);
+}
+BENCHMARK(VectorDiv)->Arg(kN);
 
 BENCHMARK_MAIN();
