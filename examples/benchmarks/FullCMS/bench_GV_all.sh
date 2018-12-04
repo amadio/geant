a63c8af5787463a1bd18f8aa0513c377cd992edd
Witold Pokorski <witoldp@Witolds-MacBook-Pro-2.local>
2018-12-04 11:43:15 +0100

    fixing bencharks scripts

--- examples/benchmarks/FullCMS/bench_GV_all.sh
+++ examples/benchmarks/FullCMS/bench_GV_all.sh
@@ -4,12 +4,12 @@
 # G=geometry
 # M=MSC
 #___________________F_P_G_M
-FullCMS_GV.run "$1" 0 0 0 0
-FullCMS_GV.run "$1" 1 0 0 0
-FullCMS_GV.run "$1" 2 0 0 0
-FullCMS_GV.run "$1" 0 1 0 0
-FullCMS_GV.run "$1" 0 2 0 0
-FullCMS_GV.run "$1" 0 0 1 0
-FullCMS_GV.run "$1" 0 0 2 0
-FullCMS_GV.run "$1" 0 0 0 1
-FullCMS_GV.run "$1" 0 0 0 2
+./FullCMS_GV.run "$1" 0 0 0 0
+./FullCMS_GV.run "$1" 1 0 0 0
+./FullCMS_GV.run "$1" 2 0 0 0
+./FullCMS_GV.run "$1" 0 1 0 0
+./FullCMS_GV.run "$1" 0 2 0 0
+./FullCMS_GV.run "$1" 0 0 1 0
+./FullCMS_GV.run "$1" 0 0 2 0
+./FullCMS_GV.run "$1" 0 0 0 1
+./FullCMS_GV.run "$1" 0 0 0 2
