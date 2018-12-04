947d53a3e7d383c397462c788cf4aa3b4cdde8db
Witold Pokorski <witoldp@Witolds-MacBook-Pro-2.local>
2018-12-04 11:19:09 +0100

    fixing call to benchmark scripts

--- examples/benchmarks/TestEm3/bench_GV_all.sh
+++ examples/benchmarks/TestEm3/bench_GV_all.sh
@@ -4,12 +4,12 @@
 # G=geometry
 # M=MSC
 #___________________F_P_G_M
-TestEm3_GV.run "$1" 0 0 0 0
-TestEm3_GV.run "$1" 1 0 0 0
-TestEm3_GV.run "$1" 2 0 0 0
-TestEm3_GV.run "$1" 0 1 0 0
-TestEm3_GV.run "$1" 0 2 0 0
-TestEm3_GV.run "$1" 0 0 1 0
-TestEm3_GV.run "$1" 0 0 2 0
-TestEm3_GV.run "$1" 0 0 0 1
-TestEm3_GV.run "$1" 0 0 0 2
+./TestEm3_GV.run "$1" 0 0 0 0
+./TestEm3_GV.run "$1" 1 0 0 0
+./TestEm3_GV.run "$1" 2 0 0 0
+./TestEm3_GV.run "$1" 0 1 0 0
+./TestEm3_GV.run "$1" 0 2 0 0
+./TestEm3_GV.run "$1" 0 0 1 0
+./TestEm3_GV.run "$1" 0 0 2 0
+./TestEm3_GV.run "$1" 0 0 0 1
+./TestEm3_GV.run "$1" 0 0 0 2
