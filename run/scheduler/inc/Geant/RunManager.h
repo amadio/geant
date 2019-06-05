28e06426d99cf17aeeaba7962e9fec940ead88d0
Philippe Canal <pcanal@fnal.gov>
2019-06-04 20:01:11 -0500

    Workaround BitSet moving from veccore to vecgeom namespace

--- run/scheduler/inc/Geant/RunManager.h
+++ run/scheduler/inc/Geant/RunManager.h
@@ -34,7 +34,15 @@ class PrimaryGenerator;
 class MCTruthMgr;
 class EventSet;
 
+// Work around bitset moving from veccore to vecgeom
+namespace bitsetworkaround {
+  using namespace veccore;
+  using namespace vecgeom;
+}
+
 class RunManager {
+  using BitSet = bitsetworkaround::BitSet;
+
 private:
   bool fInitialized    = false;
   int fNpropagators    = 0;       /** Number of propagators */
